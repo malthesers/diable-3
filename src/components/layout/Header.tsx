3de609fe879f7e6d52d03e8f8a7d37ba35e6066c
malt9830 <malt9830@stud.kea.dk>
2023-10-16 01:51:46 +0200

    Removed underlining of app name in header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,13 +8,12 @@ export default function Header() {
 
   return (
     <header className="w-full px-6 py-4 font-semibold">
-      <nav className="max-w-7xl mx-auto flex justify-between">
-        <Link href="/" className="text-xl">diable</Link>
-        <div className="flex gap-4">
+      <div className="max-w-7xl mx-auto flex justify-between">
+        <Link href="/" className="text-xl after:">Diable</Link>
+        <nav className="flex gap-4">
           <Link href="/" className={`${pathname === '/' ? 'active' : ''}`}>Diablo III</Link>
-          <Link href="/albums" className={`${pathname === '/items' ? 'active' : ''}`}>Items</Link>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </header>
   )
 }
