607b6dcaa74af021cf121fed98f878754e0061e1
malt9830 <malt9830@stud.kea.dk>
2023-10-16 04:40:54 +0200

    Added quality/armor/slot line

--- src/components/game/KnownInfo.tsx
+++ src/components/game/KnownInfo.tsx
@@ -5,20 +5,24 @@ import Image from "next/image"
 export default function KnownInfo() {
   let [known, setKnown] = useState<Item>({
     name: 'Unidentified',
-    quality: 'base'
+    quality: 'legendary'
   })
   
   return (
-    <div className="w-[330px] sm:w-[400px] mx-auto bg-black border-grey border-4 p-2 space-y-2">
+    <div className="w-[330px] sm:w-[400px] mx-auto bg-black border-grey border-4 p-1 space-y-2">
       <div className={ `bg-${known.quality}` + ' w-full aspect-[350/40] bg-no-repeat bg-cover grid p-2'}>
-        <p className="place-self-center text-lg leading-none text-red-800 tracking-wider uppercase">{known.name}</p>
+        <p className="text-red-800 text-lg leading-none tracking-wider uppercase place-self-center">{known.name}</p>
       </div>
-      <div className="flex flex-row gap-4">
-        <div className="w-20 h-40 border-grey border-2 p-2">
-          <div className="bg-orange-900 bg-opacity-50 w-full h-full grid place-content-center text-4xl">?</div>
+      <div className="grid grid-cols-[5rem_auto] border-grey border-2 gap-4 p-2">
+        <div className="h-40 p-2 border-grey border-2">
+          <div className="bg-zinc-900 w-full h-full grid place-content-center text-4xl">?</div>
         </div>
         <div className="font-sans capitalize">
-          <p><span className="">{known.quality}</span></p>
+          <p className={ `text-${known.quality}` + ' flex gap-1'}>
+            <span className="">{known.quality}</span>
+            <span>Gloves</span>
+            <span className="ml-auto text-neutral-500">Hands</span>
+          </p>
         </div>
       </div>
     </div>
