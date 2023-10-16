0d3c3579a251e83b7d1e257a5f27d35bced82d89
malt9830 <malt9830@stud.kea.dk>
2023-10-16 04:24:41 +0200

    Added quality colours to tw config

--- src/components/game/KnownInfo.tsx
+++ src/components/game/KnownInfo.tsx
@@ -9,13 +9,17 @@ export default function KnownInfo() {
   })
   
   return (
-    <div className="w-[320px] sm:w-[400px] mx-auto bg-black border-zinc-800 border-2 p-2 space-y-2">
-      <div className={ `bg-${known.quality}` + ' aspect-[350/40] bg-no-repeat bg-cover grid p-2'}>
-        <p className="place-self-center text-lg sm:text-xl text-red-800 tracking-wider uppercase">{known.name}</p>
+    <div className="w-[330px] sm:w-[400px] mx-auto bg-black border-grey border-4 p-2 space-y-2">
+      <div className={ `bg-${known.quality}` + ' w-full aspect-[350/40] bg-no-repeat bg-cover grid p-2'}>
+        <p className="place-self-center text-lg leading-none text-red-800 tracking-wider uppercase">{known.name}</p>
       </div>
-      <div className="flex flex-row gap-2">
-        <div className="w-20 h-40 border-grey border-2"></div>
-        <div></div>
+      <div className="flex flex-row gap-4">
+        <div className="w-20 h-40 border-grey border-2 p-2">
+          <div className="bg-orange-900 bg-opacity-50 w-full h-full grid place-content-center text-4xl">?</div>
+        </div>
+        <div className="font-sans capitalize">
+          <p><span className="">{known.quality}</span></p>
+        </div>
       </div>
     </div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,18 +9,28 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        grey: '#2E2921'
+        grey: '#2E2921',
+        base: '#FFFFFF',
+        magic: '#6969FF',
+        rare: '#FFFF00',
+        legendary: '#BF642F',
+        set: '#00FF00'
       },
       backgroundImage: {
-        'base': "url('/images/nameplates/base.jpg')",
-        'rare': "url('/images/nameplates/rare.jpg')",
-        'magic': "url('/images/nameplates/magic.jpg')",
-        'legendary': "url('/images/nameplates/legendary.jpg')",
-        'set': "url('/images/nameplates/set.jpg')",
+        base: "url('/images/nameplates/base.jpg')",
+        magic: "url('/images/nameplates/magic.jpg')",
+        rare: "url('/images/nameplates/rare.jpg')",
+        legendary: "url('/images/nameplates/legendary.jpg')",
+        set: "url('/images/nameplates/set.jpg')",
       },
     },
   },
   safelist: [
+    'text-base',
+    'text-rare',
+    'text-magic',
+    'text-legendary',
+    'text-set',
     'bg-base',
     'bg-rare',
     'bg-magic',
