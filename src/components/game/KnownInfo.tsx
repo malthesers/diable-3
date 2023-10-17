70ab249a4e7536b34313ef171c49d50c4d32fc8f
malt9830 <malt9830@stud.kea.dk>
2023-10-17 15:29:00 +0200

    Added icons backgrounds

--- src/components/game/KnownInfo.tsx
+++ src/components/game/KnownInfo.tsx
@@ -5,18 +5,18 @@ import Image from "next/image"
 export default function KnownInfo() {
   let [known, setKnown] = useState<Item>({
     name: 'unidentified',
-    quality: 'legendary',
+    quality: 'undefined',
     slot: 'unknown',
     type: 'something'
   })
   
   return (
     <div className="w-[330px] sm:w-[400px] mx-auto bg-black border-zinc-800 border-4 p-1 space-y-2">
-      <div className={`bg-${known.quality}` + ' w-full aspect-[350/40] bg-no-repeat bg-cover grid p-1'}>
+      <div className={`bg-${known.quality}-name` + ' w-full aspect-[350/40] bg-no-repeat bg-cover grid p-1'}>
         <p className="text-red-800 pt-1 text-lg sm:text-xl leading-none tracking-wider uppercase place-self-center">{known.name}</p>
       </div>
       <div className="grid grid-cols-[5rem_auto] border-grey border-2 gap-4 p-2">
-        <div className="h-36 bg-[#BF642F] border-grey border-2 rounded">
+        <div className="h-36 border-grey border-2 rounded">
           <div className="w-full h-full grid place-content-center text-4xl">?</div>
         </div>
         <div className="font-sans capitalize">
