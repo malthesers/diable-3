646c084d5f11a237c02743c47e0f138bcee2300f
malt9830 <malt9830@stud.kea.dk>
2023-10-18 02:47:09 +0200

    Added input value for input field

--- src/components/game/ItemSearch.tsx
+++ src/components/game/ItemSearch.tsx
@@ -1,13 +1,19 @@
+'use client'
+
 import { Item } from "@/src/interfaces/item"
+import { useRef } from "react"
 
 export default function ItemSearch({ items }: { items:Array<Item>}) {
+  const input = useRef(null)
+  const results:Item[] = items.filter(item => item.name.includes('a'))
+
   return (
     <div>
       <label htmlFor='item-search' className="w-[330px] sm:w-[400px] bg-undefined-name bg-no-repeat bg-cover aspect-[350/40] grid p-1">
-        <input name="item-search" placeholder="Enter item name..." className="bg-transparent text-center uppercase focus:outline-none"></input>
+        <input ref={input} type="text" name="item-search" placeholder="Enter item name..." className="bg-transparent text-center uppercase focus:outline-none"></input>
       </label>
       <div className="flex flex-wrap">
-        { items.map((item) =>
+        { results && results.map((item) =>
           <p key={item.name}>{item.name}</p>
         )}
       </div>
