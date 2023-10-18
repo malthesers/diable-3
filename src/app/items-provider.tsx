df487004986515c45910a3378e8c615fa9de6768
malt9830 <malt9830@stud.kea.dk>
2023-10-18 03:39:48 +0200

    Moved index states to context provider

--- src/app/items-provider.tsx
+++ src/app/items-provider.tsx
@@ -1,14 +1,74 @@
 'use client'
 
-import { ReactNode, createContext } from "react"
+import { ReactNode, createContext, useContext, useState, useEffect } from "react"
+import { Item } from "../interfaces/item"
+
 
 export const ItemsContext = createContext({})
+export function useItems() {
+  return useContext(ItemsContext)
+}
 
 export default function ItemsProvider({ children }: { children: ReactNode }) {
-  const items = ['yo']
+  const [answer, setAnswer] = useState<Item | null>(null)
+  const [guesses, setGuesses] = useState<Item[]>([])
+  const [known, setKnown] = useState<Item>({
+    name: 'unidentified',
+    quality: 'undefined',
+    slot: 'unknown',
+    type: 'something'
+  })
+  const [items, setItems] = useState<Item[]>([
+    {
+      name: 'Dawn',
+      class: 'demon hunter',
+      quality: 'legendary',
+      slot: '1-hand',
+      type: 'hand crossbow'
+    },
+    {
+      name: 'Valla\'s Bequest',
+      class: 'demon hunter',
+      quality: 'legendary',
+      slot: '1-hand',
+      type: 'hand crossbow'
+    },
+    {
+      name: 'Natalya\'s Slayer',
+      class: 'demon hunter',
+      quality: 'set',
+      slot: '1-hand',
+      type: 'hand crossbow'
+    },
+    {
+      name: 'Penetrator',
+      class: 'demon hunter',
+      quality: 'rare',
+      slot: '1-hand',
+      type: 'hand crossbow'
+    },
+    {
+      name: 'Hand Crossbow',
+      class: 'demon hunter',
+      quality: 'magic',
+      slot: '1-hand',
+      type: 'hand crossbow'
+    },
+    {
+      name: 'Initiate\'s Hand Crossbow',
+      class: 'demon hunter',
+      quality: 'common',
+      slot: '1-hand',
+      type: 'hand crossbow'
+    },
+  ])
+
+  useEffect(() => {
+    setAnswer(items[Math.floor(Math.random() * items.length)])
+  }, [])
 
   return (
-    <ItemsContext.Provider value={{ items }}>
+    <ItemsContext.Provider value={{ items, answer, guesses, known }}>
       {children}
     </ItemsContext.Provider>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,65 +5,67 @@ import { Item } from "../interfaces/item"
 import PastGuesses from "../components/game/PastGuesses"
 import ItemSearch from "../components/game/ItemSearch"
 import ItemBox from "../components/game/ItemBox"
+import { useItems } from "./items-provider"
 
 export default function Home() {
   const [answer, setAnswer] = useState<Item | null>(null)
-  const [guesses, setGuesses] = useState<Item[]>([])
-  const [known, setKnown] = useState<Item>({
-    name: 'unidentified',
-    quality: 'undefined',
-    slot: 'unknown',
-    type: 'something'
-  })
+  const { known } = useItems()
+  // const [guesses, setGuesses] = useState<Item[]>([])
+  // const [known, setKnown] = useState<Item>({
+  //   name: 'unidentified',
+  //   quality: 'undefined',
+  //   slot: 'unknown',
+  //   type: 'something'
+  // })
 
-  const [items, setItems] = useState<Item[]>([
-    {
-      name: 'Dawn',
-      class: 'demon hunter',
-      quality: 'legendary',
-      slot: '1-hand',
-      type: 'hand crossbow'
-    },
-    {
-      name: 'Valla\'s Bequest',
-      class: 'demon hunter',
-      quality: 'legendary',
-      slot: '1-hand',
-      type: 'hand crossbow'
-    },
-    {
-      name: 'Natalya\'s Slayer',
-      class: 'demon hunter',
-      quality: 'set',
-      slot: '1-hand',
-      type: 'hand crossbow'
-    },
-    {
-      name: 'Penetrator',
-      class: 'demon hunter',
-      quality: 'rare',
-      slot: '1-hand',
-      type: 'hand crossbow'
-    },
-    {
-      name: 'Hand Crossbow',
-      class: 'demon hunter',
-      quality: 'magic',
-      slot: '1-hand',
-      type: 'hand crossbow'
-    },
-    {
-      name: 'Initiate\'s Hand Crossbow',
-      class: 'demon hunter',
-      quality: 'common',
-      slot: '1-hand',
-      type: 'hand crossbow'
-    },
-  ])
+  // const [items, setItems] = useState<Item[]>([
+  //   {
+  //     name: 'Dawn',
+  //     class: 'demon hunter',
+  //     quality: 'legendary',
+  //     slot: '1-hand',
+  //     type: 'hand crossbow'
+  //   },
+  //   {
+  //     name: 'Valla\'s Bequest',
+  //     class: 'demon hunter',
+  //     quality: 'legendary',
+  //     slot: '1-hand',
+  //     type: 'hand crossbow'
+  //   },
+  //   {
+  //     name: 'Natalya\'s Slayer',
+  //     class: 'demon hunter',
+  //     quality: 'set',
+  //     slot: '1-hand',
+  //     type: 'hand crossbow'
+  //   },
+  //   {
+  //     name: 'Penetrator',
+  //     class: 'demon hunter',
+  //     quality: 'rare',
+  //     slot: '1-hand',
+  //     type: 'hand crossbow'
+  //   },
+  //   {
+  //     name: 'Hand Crossbow',
+  //     class: 'demon hunter',
+  //     quality: 'magic',
+  //     slot: '1-hand',
+  //     type: 'hand crossbow'
+  //   },
+  //   {
+  //     name: 'Initiate\'s Hand Crossbow',
+  //     class: 'demon hunter',
+  //     quality: 'common',
+  //     slot: '1-hand',
+  //     type: 'hand crossbow'
+  //   },
+  // ])
 
-  useEffect(() => {
-    setAnswer(items[Math.floor(Math.random() * items.length)])
-  }, [])
+  // useEffect(() => {
+  //   setAnswer(items[Math.floor(Math.random() * items.length)])
+  // }, [])
 
   return (
     <main>
@@ -73,8 +75,8 @@ export default function Home() {
           { answer && <ItemBox item={answer}/> }
         </div>
         <div>
-          <ItemSearch items={items} />
-          <PastGuesses items={guesses}/>
+          {/* <ItemSearch items={items} />
+          <PastGuesses items={guesses}/> */}
         </div>
       </section>
     </main>
