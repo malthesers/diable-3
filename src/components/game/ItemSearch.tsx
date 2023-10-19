abccb9d51ff0327a84ab2c7fa8d097660062fbcc
malt9830 <malt9830@stud.kea.dk>
2023-10-19 20:02:09 +0200

    Fixed label-input error

--- src/components/game/ItemSearch.tsx
+++ src/components/game/ItemSearch.tsx
@@ -14,7 +14,7 @@ export default function ItemSearch() {
   return (
     <div>
       <label htmlFor='item-search' className='w-[315px] sm:w-[350px] md:w-[385px] bg-undefined-name bg-no-repeat bg-cover aspect-[350/40] grid p-1 mb-2'>
-        <input value={search} onChange={e => setSearch(e.target.value)} type='text' name='item-search' placeholder='Enter item name...' className='bg-transparent text-center uppercase focus:outline-none'></input>
+        <input value={search} onChange={e => setSearch(e.target.value)} type='text' id='item-search' name='item-search' placeholder='Enter item name...' className='bg-transparent text-center uppercase focus:outline-none'></input>
       </label>
       <div className='absolute w-[315px] sm:w-[350px] md:w-[385px] h-fit mx-auto flex flex-col gap-1'>
         { 0 < search.length && results.map((item) =>
