4e99bca51339252fff473c5d70ef6ba61670e079
malt9830 <malt9830@stud.kea.dk>
2023-10-19 16:19:21 +0200

    Re-factored Item interface

--- src/interfaces/item.d.ts
+++ src/interfaces/item.d.ts
@@ -2,13 +2,6 @@ type Classes = 'barbarian' | 'crusader' | 'demon hunter' | 'monk' | 'necromancer
 type Qualities = 'undefined' | 'base' | 'common' | 'magic' | 'rare' | 'legendary' | 'set'
 type Slots = 'unknown' | 'head' | 'shoulders' | 'torso' | 'wrists' | 'hands' | 'waist' | 'legs' | 'feet' | 'neck' | 'finger' | 'off-hand' | '1-hand' | '2-hand'
 
-type Head = 'helm' | 'spirit stone' | 'voodoo mask' | 'wizard hat'
-type Torso = 'chest armor' | 'cloak'
-type Waist = 'belt' | 'mighty belt'
-type OffHand = 'shield' | 'crusader shield' | 'mojo' | 'source' | 'quiver' | 'phylactery'
-type TwoHand = 'axe' | 'mace' | 'polearm' | 'staff' | 'sword' | 'daibo' | 'flail' | 'mighty weapon' | 'scythe' | 'bow' | 'crossbow'
-type OneHand = 'axe' | 'dagger' | 'mace' | 'spear' | 'sword' | 'ceremonial knife' | 'fist weapon' | 'flail' | 'mighty weapon' | 'hand crossbow' | 'wand'
-
 type Types<TSlot> =
   TSlot extends 'head' ? Head :
   TSlot extends 'shoulders' ? 'shoulders' :
@@ -25,10 +18,82 @@ type Types<TSlot> =
   TSlot extends '1-hand' ? OneHand :
   'something';
 
-export interface Item {
+export interface TItem {
   name: string,
   class?: Classes,
   quality: Qualities,
   slot: Slots,
   type: Types<Item['slot']>
+}
+
+export interface Item {
+  name: string,
+  class?: Classes,
+  quality: Qualities,
+  equipment: Head | Shoulders | Torso | Wrists | Hands | Waist | Legs | Feet | Neck | Finger | OffHand | TwoHand | OneHand
+} 
+
+type Head = {
+  slot: 'head'
+  type: 'helm' | 'spirit stone' | 'voodoo mask' | 'wizard hat'
+}
+
+type Shoulders = {
+  slot: 'shoulders'
+  type: 'shoulders'
+}
+
+type Torso = {
+  slot: 'torso'
+  type: 'chest armor' | 'cloak'
+}
+
+type Wrists = {
+  slot: 'wrists'
+  type: 'bracers'
+}
+
+type Hands = {
+  slot: 'hands'
+  type: 'gloves'
+}
+
+type Waist = {
+  slot: 'waist'
+  type: 'belt' | 'mighty belt'
+}
+
+type Legs = {
+  slot: 'legs'
+  type: 'pants'
+}
+
+type Feet = {
+  slot: 'feet'
+  type: 'boots'
+}
+
+type Neck = {
+  slot: 'neck'
+  type: 'amulet'
+}
+
+type Finger = {
+  slot: 'finger'
+  type: 'ring'
+}
+
+type OffHand = {
+  slot: 'off-hand'
+  type: 'shield' | 'crusader shield' | 'mojo' | 'source' | 'quiver' | 'phylactery'
+}
+
+type TwoHand = {
+  slot: '2-hand'
+  type: 'axe' | 'mace' | 'polearm' | 'staff' | 'sword' | 'daibo' | 'flail' | 'mighty weapon' | 'scythe' | 'bow' | 'crossbow'
+}
+
+type OneHand = {
+  slot: '1-hand'
+  type: 'axe' | 'dagger' | 'mace' | 'spear' | 'sword' | 'ceremonial knife' | 'fist weapon' | 'flail' | 'mighty weapon' | 'hand crossbow' | 'wand'
 }
