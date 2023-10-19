a9d1ca2fb30de9dded179aecdd2619c0c7153de9
malt9830 <malt9830@stud.kea.dk>
2023-10-19 20:16:57 +0200

    Fixed search item margins

--- src/components/game/ItemSearch.tsx
+++ src/components/game/ItemSearch.tsx
@@ -16,11 +16,11 @@ export default function ItemSearch() {
   }
 
   return (
-    <div>
-      <label htmlFor='item-search' className='w-[315px] sm:w-[350px] md:w-[385px] bg-undefined-name bg-no-repeat bg-cover aspect-[350/40] grid p-1 mb-2'>
+    <div className='relative w-[315px] sm:w-[350px] md:w-[385px] mx-auto'>
+      <label htmlFor='item-search' className='mx-auto bg-undefined-name bg-no-repeat bg-cover aspect-[350/40] grid p-1 mb-2'>
         <input value={search} onChange={e => setSearch(e.target.value)} type='text' id='item-search' name='item-search' placeholder='Enter item name...' className='bg-transparent text-center uppercase focus:outline-none'></input>
       </label>
-      <div className='absolute w-[315px] sm:w-[350px] md:w-[385px] h-fit mx-auto flex flex-col gap-1'>
+      <div className='absolute w-full h-fit mx-auto flex flex-col gap-1 bg-black'>
         { 0 < search.length && results.map((item) =>
           <article onClick={() => submitGuess(item)} key={item.name} className='cursor-pointer shadow-item-inner'>
             <ItemNameplate item={item} hover={true}/>
