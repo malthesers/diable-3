01e35349ff23f26af2063481099c0d76c910a3a2
malt9830 <malt9830@stud.kea.dk>
2023-10-19 20:25:23 +0200

    Re-focuses input field after submitting guess

--- src/components/game/ItemSearch.tsx
+++ src/components/game/ItemSearch.tsx
@@ -2,23 +2,25 @@
 
 import { useItems } from '@/src/app/items-provider'
 import { Item } from '@/src/interfaces/item'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import ItemNameplate from './ItemNameplate'
 
 export default function ItemSearch() {
   const { items, addGuess } = useItems()
   const [search, setSearch] = useState<string>('')
+  const input = useRef<HTMLInputElement | null>(null)
   const results:Item[] = items.filter(item => item.name.toLowerCase().includes(search.toLowerCase())).slice(0, 10)
 
   function submitGuess(item:Item) {
-    setSearch('')
+    if (input.current) input.current.focus()
     addGuess(item)
+    setSearch('')
   }
 
   return (
     <div className='relative w-[315px] sm:w-[350px] md:w-[385px] mx-auto'>
       <label htmlFor='item-search' className='mx-auto bg-undefined-name bg-no-repeat bg-cover aspect-[350/40] grid p-1 mb-2'>
-        <input value={search} onChange={e => setSearch(e.target.value)} type='text' id='item-search' name='item-search' placeholder='Enter item name...' className='bg-transparent text-center uppercase focus:outline-none'></input>
+        <input value={search} onChange={e => setSearch(e.target.value)} ref={input} type='text' id='item-search' name='item-search' placeholder='Enter item name...' className='bg-transparent text-center uppercase focus:outline-none'></input>
       </label>
       <div className='absolute w-full h-fit mx-auto flex flex-col gap-1 bg-black'>
         { 0 < search.length && results.map((item) =>
