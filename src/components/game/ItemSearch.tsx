78dd53160a8d93060b56b12d3566105d43556318
malt9830 <malt9830@stud.kea.dk>
2023-10-20 05:55:11 +0200

    Aligned left and right padding columns

--- src/components/game/ItemSearch.tsx
+++ src/components/game/ItemSearch.tsx
@@ -19,10 +19,12 @@ export default function ItemSearch() {
 
   return (
     <div className='relative w-[315px] sm:w-[350px] md:w-[385px] mx-auto'>
-      <label htmlFor='item-search' className='mx-auto bg-undefined-name bg-no-repeat bg-cover aspect-[350/40] grid p-1 mb-2'>
-        <input value={search} onChange={e => setSearch(e.target.value)} ref={input} type='text' id='item-search' name='item-search' placeholder='Enter item name...' className='bg-transparent text-center uppercase focus:outline-none'></input>
-      </label>
-      <div className='absolute w-full h-fit mx-auto flex flex-col gap-1 bg-black'>
+      <div className='p-2'>
+        <label htmlFor='item-search' className='mx-auto bg-undefined-name bg-no-repeat bg-cover aspect-[350/40] grid'>
+          <input value={search} onChange={e => setSearch(e.target.value)} ref={input} type='text' id='item-search' name='item-search' placeholder='Enter item name...' className='bg-transparent text-center uppercase focus:outline-none'></input>
+        </label>
+      </div>
+      <div className='absolute w-full h-fit mx-auto flex flex-col gap-1 px-2'>
         { 0 < search.length && results.map((item) =>
           <article
             key={item.name} tabIndex={0}
