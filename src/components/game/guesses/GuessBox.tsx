4466efb51181f4374a3e7c82fd314174d01847f6
malt9830 <malt9830@stud.kea.dk>
2023-10-21 21:20:26 +0200

    Updated background for GuessBox

--- src/components/game/guesses/GuessBox.tsx
+++ src/components/game/guesses/GuessBox.tsx
@@ -5,11 +5,11 @@ interface ItemGuessProps {
 
 export default function GuessBox({ guess, answer }: ItemGuessProps) {
   const correct:boolean = guess === answer
-  const background:string = guess === answer ? 'set' : 'legendary'
+  const background:string = correct ? 'set' : 'base'
 
   return (
     <div className={`grid aspect-square bg-[url("/images/icons/${background}.png")] bg-center bg-cover`}>
-      <div className={(correct ? 'bg-green-700' : 'bg-red-700') + ' w-full h-full bg-opacity-40 grid-center'}></div>
+      <div className={(correct ? 'bg-green-600' : 'bg-red-600') + ' w-full h-full bg-opacity-40 grid-center'}></div>
       <span className='grid-center my-auto'>{guess}</span>
     </div>
   )
