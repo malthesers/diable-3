8f286ff7b292fef366160c78e2e801b3528cea27
malt9830 <malt9830@stud.kea.dk>
2023-10-21 23:16:31 +0200

    Fixed GuessBox referencing wrong bg-image

--- src/components/game/guesses/GuessBox.tsx
+++ src/components/game/guesses/GuessBox.tsx
@@ -7,8 +7,8 @@ export default function GuessBox({ guess, answer }: ItemGuessProps) {
   const correct:boolean = guess === answer
 
   return (
-    <div className={(correct ? 'bg-set-icon' : 'bg-base-icon') + ` grid h-20 bg-center bg-cover`}>
-      <div className={(correct ? 'bg-green-600' : 'bg-red-600') + ' w-full h-full bg-opacity-40 grid-center shadow-guess-inner'}></div>
+    <div className={(correct ? 'bg-set-icon' : 'bg-undefined-icon') + ` grid h-20 bg-center bg-cover`}>
+      <div className={(correct ? 'bg-green-600' : 'bg-red-800') + ' w-full h-full bg-opacity-40 grid-center shadow-guess-inner'}></div>
       <span className='my-auto text-sm grid-center'>{guess}</span>
     </div>
   )
