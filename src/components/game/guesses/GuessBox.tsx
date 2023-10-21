343bc28b28ffcffec3f56fc3a3ed0c0d09d276f9
malt9830 <malt9830@stud.kea.dk>
2023-10-21 21:12:15 +0200

    Added correct boolean to GuessBox

--- src/components/game/guesses/GuessBox.tsx
+++ src/components/game/guesses/GuessBox.tsx
@@ -4,9 +4,11 @@ interface ItemGuessProps {
 }
 
 export default function GuessBox({ guess, answer }: ItemGuessProps) {
+  const correct:boolean = guess === answer
+
   return (
     <div className='grid aspect-square bg-[url("/images/icons/set.png")] bg-center bg-cover'>
-      <div className={(guess === answer ? 'bg-green-700' : 'bg-red-700') + ' w-full h-full bg-opacity-40 grid-center'}></div>
+      <div className={(correct ? 'bg-green-700' : 'bg-red-700') + ' w-full h-full bg-opacity-40 grid-center'}></div>
       <span className='grid-center my-auto'>{guess}</span>
     </div>
   )
