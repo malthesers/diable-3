6a35adea978cd30a8350debfc0c2fdab4de76f49
malt9830 <malt9830@stud.kea.dk>
2023-10-21 21:13:43 +0200

    Added background property based on correctness

--- src/components/game/guesses/GuessBox.tsx
+++ src/components/game/guesses/GuessBox.tsx
@@ -5,9 +5,10 @@ interface ItemGuessProps {
 
 export default function GuessBox({ guess, answer }: ItemGuessProps) {
   const correct:boolean = guess === answer
+  const background:string = guess === answer ? 'set' : 'legendary'
 
   return (
-    <div className='grid aspect-square bg-[url("/images/icons/set.png")] bg-center bg-cover'>
+    <div className={`grid aspect-square bg-[url("/images/icons/${background}.png")] bg-center bg-cover`}>
       <div className={(correct ? 'bg-green-700' : 'bg-red-700') + ' w-full h-full bg-opacity-40 grid-center'}></div>
       <span className='grid-center my-auto'>{guess}</span>
     </div>
