02086e9b704698621d2ddabf05c99ed27c38e348
malt9830 <malt9830@stud.kea.dk>
2023-10-22 19:16:24 +0200

    Removed Guess subtitle

--- src/components/game/PastGuesses.tsx
+++ src/components/game/PastGuesses.tsx
@@ -5,13 +5,10 @@ export default function PastGuesses() {
   const { guesses } = useItems()
 
   return (
-    <div>
-      <p className='text-2xl text-center'>Guesses</p>
-      <div className='flex flex-col gap-2'>
-        { guesses.map((item) =>
-          <ItemGuess key={item.name} item={item}/>
-        )}
-      </div>
+    <div className='flex flex-col gap-2'>
+      { guesses.map((item) =>
+        <ItemGuess key={item.name} item={item}/>
+      )}
     </div>
   )
 }
