49200cc7ecc00ff5703239abdf9e0f0ab2efa952
malt9830 <malt9830@stud.kea.dk>
2023-10-23 00:45:37 +0200

    Installed TransitionGroup types

--- src/components/game/ItemBox.tsx
+++ src/components/game/ItemBox.tsx
@@ -1,8 +1,15 @@
+import { useRef } from 'react'
+import { SwitchTransition } from 'react-transition-group'
 import { Item } from '@/src/interfaces/item'
 import ItemNameplate from './ItemNameplate'
 import Image from 'next/image'
 
 export default function ItemRow({ item }: { item: Item}) {
+  const itemQuality = useRef(null)
+  const itemClass = useRef(null)
+  const itemType = useRef(null)
+  const itemSlot = useRef(null)
+
   return (
     <div className='h-fit mx-auto bg-black border-zinc-800 border-2 p-1 space-y-2'>
       <ItemNameplate item={item}/>
@@ -20,7 +27,9 @@ export default function ItemRow({ item }: { item: Item}) {
         </div>
         <div className='font-sans capitalize md:text-lg'>
           <p className={`text-${item.quality}` + ' flex flex-col md:flex-row gap-1'}>
-            <span>{item.quality}</span>
+            <SwitchTransition ref={itemQuality} in={true} timeout={200} classNames='out-in'>
+              <span ref={itemQuality}>{item.quality}</span>
+            </SwitchTransition>
             <span>{item.equipment.type}</span>
             <span className='md:ml-auto text-neutral-500'>{item.equipment.slot}</span>
           </p>
