099cc56cefd60dde832e7781666c85a04972e43e
malt9830 <malt9830@stud.kea.dk>
2023-10-22 19:42:26 +0200

    Past guesses fade out while searching

--- src/components/game/PastGuesses.tsx
+++ src/components/game/PastGuesses.tsx
@@ -2,10 +2,10 @@ import { useItems } from '@/src/app/items-provider'
 import ItemGuess from './guesses/ItemGuess'
 
 export default function PastGuesses() {
-  const { guesses } = useItems()
+  const { guesses, search } = useItems()
 
   return (
-    <div className='flex flex-col gap-2'>
+    <div className={(0 < search.length && 'brightness-50') + ' flex flex-col gap-2'}>
       { guesses.map((item) =>
         <ItemGuess key={item.name} item={item}/>
       )}
