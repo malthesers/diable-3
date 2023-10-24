2c6a9b8435ff1b6c133b48ed91e51ccb4373c8d6
malt9830 <malt9830@stud.kea.dk>
2023-10-24 02:19:21 +0200

    Renamed to Diable III/3

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,10 +9,10 @@ export default function Header() {
   return (
     <header className='w-full bg-black bg-opacity-20 px-6 py-4 font-semibold'>
       <div className='max-w-7xl mx-auto flex justify-between'>
-        <Link href='/' className='text-xl after:'>Diable</Link>
-        <nav className='flex gap-4'>
+        <Link href='/' className='text-xl after:'>Diable III</Link>
+        {/* <nav className='flex gap-4'>
           <Link href='/' className={`${pathname === '/' ? 'active' : ''}`}>Diablo III</Link>
-        </nav>
+        </nav> */}
       </div>
     </header>
   )
