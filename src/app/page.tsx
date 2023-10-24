a58e0fde7ed31fc8665e146b6a508b3293f576cc
malt9830 <malt9830@stud.kea.dk>
2023-10-24 23:33:21 +0200

    Removed redundant margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           <ItemBox item={known}/>
           { answer && <ItemBox item={answer}/> }
         </div>
-        <div className='flex flex-col gap-2 place-items-center p-1 border-2 border-transparent'>
+        <div className='flex flex-col place-items-center p-1 border-2 border-transparent'>
           <ItemSearch/>
           <PastGuesses/>
         </div>
