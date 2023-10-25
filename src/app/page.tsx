3cf454b2609c2f6453c79cf8c74f3fdfa84683bc
malt9830 <malt9830@stud.kea.dk>
2023-10-25 05:09:04 +0200

    Added button to display instructions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,19 @@ import ModalsContainer from '../components/modals/ModalsContainer'
 import ItemSearch from '@/src/components/game/search/ItemSearch'
 import ItemBox from '@/src/components/game/known/ItemBox'
 import Button from '@/src/components/layout/Button'
+import { useModals } from '../context/ModalsProvider'
 import { useItems } from '../context/ItemsProvider'
 import { createPortal } from 'react-dom'
 
 export default function Home() {
   const { answer, known, resetGame } = useItems()
+  const { toggleShowInstructions } = useModals()
 
   return (
     <main>
       <section className='max-w-[400px] sm:max-w-[430px] md:max-w-[920px] lg:max-w-none mx-auto flex flex-row gap-2 mb-2 px-2 lg:px-3'>
         <Button onClick={resetGame} text='New Game'/>
+        <Button onClick={() => toggleShowInstructions(true)} text='How to play'/>
       </section>
       <section className='grid md:grid-cols-2'>
         <div className='h-fit grid gap-2'>
