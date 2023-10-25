b379eb28575354b32dd198ba2e3a33c93bbf3a67
malt9830 <malt9830@stud.kea.dk>
2023-10-25 04:54:33 +0200

    Wrong guesses fade when correct answer is made

--- src/components/game/guesses/ItemGuess.tsx
+++ src/components/game/guesses/ItemGuess.tsx
@@ -4,10 +4,11 @@ import ItemNameplate from '../ItemNameplate'
 import GuessBox from './GuessBox'
 
 export default function ItemGuess({ item }: { item: Item}) {
-  const { answer } = useItems()
+  const { answer, known } = useItems()
+  const wasGuessed = answer?.name === known.name
 
   return (
-    <div className='nameplate h-fit'>
+    <article className={(wasGuessed ? '[&:not(:first-child)]:opacity-50 ' : '') + 'nameplate h-fit'}>
       <ItemNameplate item={item}/>
       <div className='grid grid-cols-4 gap-1 sm:gap-2 text-center uppercase'>
         <GuessBox guess={item.quality} answer={answer?.quality}/>
@@ -15,6 +16,6 @@ export default function ItemGuess({ item }: { item: Item}) {
         <GuessBox guess={item.equipment.type} answer={answer?.equipment.type}/>
         <GuessBox guess={item.class} answer={answer?.class}/>
       </div>
-    </div>
+    </article>
   )
 }
