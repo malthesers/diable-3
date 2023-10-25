df5140460395e6e6f48571b6553013edd090ba2d
malt9830 <malt9830@stud.kea.dk>
2023-10-25 02:36:49 +0200

    Added button to modal

--- src/components/modals/GuessedModal.tsx
+++ src/components/modals/GuessedModal.tsx
@@ -1,9 +1,14 @@
+import Button from "../layout/Button";
+
 export default function GuessedModal() {
   return (
     <aside className='fixed top-0 w-full h-[100dvh] grid p-4'>
       <div className="max-w-xl w-full bg-black text-white m-auto p-4">
         <p className="text-2xl text-center">Item was guessed!</p>
         <p>Lorem ipsum dolor sit amet</p>
+        <div className="grid place-content-center mt-4">
+          <Button onClick={() => console.log('yo')} text="Close"/>
+        </div>
       </div>
     </aside>
   )
