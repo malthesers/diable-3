d11da7e1c920b99a1e431f46a6e81b9f5339148b
malt9830 <malt9830@stud.kea.dk>
2023-10-25 04:33:27 +0200

    VictoryModal now displays amount of guesses

--- src/components/modals/VictoryModal.tsx
+++ src/components/modals/VictoryModal.tsx
@@ -4,14 +4,18 @@ import ModalTemplate from "./ModalTemplate";
 
 export default function VictoryModal() {
   const { showVictory, toggleShowVictory } = useModals()
-  const { answer } = useItems()
+  const { answer, guesses } = useItems()
 
   return (
     <ModalTemplate showModal={showVictory} closeModal={() => toggleShowVictory(false)} title="Item was guessed!">
       <p className="text-xl text-center">
-        <span>You succesfully guessed </span>
-        <span className={`text-${answer?.quality}`}>{answer?.name}</span>
-        <span>in 5 guesses.</span>
+        <span>You succesfully guessed</span>
+        <span className={`text-${answer?.quality}`}> {answer?.name} </span>
+        <span>
+          <span>in</span>
+          <span className={`text-${answer?.quality}`}> {guesses.length} </span>
+          <span>guesses.</span>
+        </span>
       </p>
     </ModalTemplate>
   )
