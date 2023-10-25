3bef1f175204acbb16cab0420cb986fae2467153
malt9830 <malt9830@stud.kea.dk>
2023-10-25 04:28:52 +0200

    Added answer to victory modal

--- src/components/modals/VictoryModal.tsx
+++ src/components/modals/VictoryModal.tsx
@@ -1,12 +1,18 @@
-import ModalTemplate from "./ModalTemplate";
 import { useModals } from "@/src/context/ModalsProvider";
+import { useItems } from "@/src/context/ItemsProvider";
+import ModalTemplate from "./ModalTemplate";
 
 export default function VictoryModal() {
   const { showVictory, toggleShowVictory } = useModals()
+  const { answer } = useItems()
 
   return (
     <ModalTemplate showModal={showVictory} closeModal={() => toggleShowVictory(false)} title="Item was guessed!">
-      <p>yo</p>
+      <p className="text-xl text-center">
+        <span>You succesfully guessed </span>
+        <span className={`text-${answer?.quality}`}>{answer?.name}</span>
+        <span>in 5 guesses.</span>
+      </p>
     </ModalTemplate>
   )
 }
