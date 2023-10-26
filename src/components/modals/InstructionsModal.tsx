894470f1d96526abaebf1b7de76ade13d0f93289
malt9830 <malt9830@stud.kea.dk>
2023-10-26 05:14:58 +0200

    Added legendary power info to instructions modal

--- src/components/modals/InstructionsModal.tsx
+++ src/components/modals/InstructionsModal.tsx
@@ -7,10 +7,9 @@ export default function InstructionsModal() {
   return (
     <ModalTemplate showModal={showInstructions} closeModal={() => toggleShowInstructions(false)} title='Instructions'>
       <div className='text-lg sm:text-xl text-center space-y-2'>
-        <p>Search for an item and enter your guess.</p>
-        <p>View guess to find correct and incorrect properties.</p>
+        <p>Search for an item and enter your guess. View guess to find correct and incorrect properties.</p>
         <p>A <span className='text-green-600'>green</span> background indicates a correct property, and a <span className='text-red-600'>red</span> background indicates an incorrect property.</p>
-        <p>Additionally revealed properties appear in the item tooltip.</p>
+        <p>Revealed properties appear in the item tooltip. After 10 guesses, a blurred <span className='text-legendary'>legendary power</span> will also appear, and be revealed on hover.</p>
         <p>Narrow down the possibilities until you find the right item!</p>
       </div>
     </ModalTemplate>
