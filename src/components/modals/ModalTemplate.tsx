337845a047011c70f8aa60fa21f9b2ec3847b2a1
malt9830 <malt9830@stud.kea.dk>
2023-10-26 02:40:05 +0200

    Added initial modal transition

--- src/components/modals/ModalTemplate.tsx
+++ src/components/modals/ModalTemplate.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import { ReactNode, useRef } from 'react';
 import Button from '../layout/Button';
 
 interface ModalProps {
@@ -9,20 +10,22 @@ interface ModalProps {
 }
 
 export default function ModalTemplate({ closeModal, showModal, children, title }: ModalProps) {
-  if (!showModal) return null
+  const modal = useRef<HTMLElement>(null)
   
   return (
-    <aside className='fixed z-50 top-0 w-full h-[100dvh] bg-black bg-opacity-20 grid p-4'>
-      <div className='max-w-xl w-full grid m-auto bg-undefined-icon bg-center border-bronze border-2'>
-        <div className='grid-center w-full h-full bg-black bg-opacity-70'></div>
-        <div className='grid-center p-4 text-white'>
-          <p className='text-2xl text-center mb-2'>{title}</p>
-          <>{children}</>
-          <div className='grid place-content-center mt-4'>
-            <Button onClick={() => closeModal()} text='Close'/>
+    <CSSTransition nodeRef={modal} in={showModal} timeout={500} classNames='fade' unmountOnExit>
+      <aside ref={modal} className='fixed z-50 top-0 w-full h-[100dvh] bg-black bg-opacity-20 grid p-4'>
+        <div className='max-w-xl w-full grid m-auto bg-undefined-icon bg-center border-bronze border-2'>
+          <div className='grid-center w-full h-full bg-black bg-opacity-70'></div>
+          <div className='grid-center p-4 text-white'>
+            <p className='text-2xl text-center mb-2'>{title}</p>
+            <>{children}</>
+            <div className='grid place-content-center mt-4'>
+              <Button onClick={() => closeModal()} text='Close'/>
+            </div>
           </div>
         </div>
-      </div>
-    </aside>
+      </aside>
+    </CSSTransition>
   )
 }
