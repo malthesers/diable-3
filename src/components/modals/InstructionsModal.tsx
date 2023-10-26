3d55c45cbaa52449b989d8400f7a4ed52e5d587c
malt9830 <malt9830@stud.kea.dk>
2023-10-26 03:03:17 +0200

    Added preliminary instructions text to modal

--- src/components/modals/InstructionsModal.tsx
+++ src/components/modals/InstructionsModal.tsx
@@ -1,16 +1,17 @@
 import { useModals } from '@/src/context/ModalsProvider';
-import { useItems } from '@/src/context/ItemsProvider';
 import ModalTemplate from './ModalTemplate';
 
 export default function InstructionsModal() {
   const { showInstructions, toggleShowInstructions } = useModals()
-  const { answer, guesses } = useItems()
 
   return (
     <ModalTemplate showModal={showInstructions} closeModal={() => toggleShowInstructions(false)} title='Instructions'>
-      <p className='text-xl text-center'>
-        <span>yoyoyoyoyo</span>
-      </p>
+      <div className='text-lg sm:text-xl text-center space-y-2'>
+        <p>Search for an item using the input field and choose your guess.</p>
+        <p>View your past guesses to find correct and incorrect values.</p>
+        <p>A <span className='text-green-600'>green</span> background indicates a correct value, and a <span className='text-red-600'>red</span> background indicates an incorrect value.</p>
+        <p>Narrow down the possibilities until you find the right item!</p>
+      </div>
     </ModalTemplate>
   )
 }
