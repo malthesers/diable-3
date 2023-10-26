3d21f31635aa3f5d0c6097f7c70b0fbc0a4f59e8
malt9830 <malt9830@stud.kea.dk>
2023-10-26 02:52:08 +0200

    Clicking outside modal also closes

--- src/components/modals/ModalTemplate.tsx
+++ src/components/modals/ModalTemplate.tsx
@@ -14,7 +14,8 @@ export default function ModalTemplate({ closeModal, showModal, children, title }
   
   return (
     <CSSTransition nodeRef={modal} in={showModal} timeout={500} classNames='fade' unmountOnExit>
-      <aside ref={modal} className='fixed z-50 top-0 w-full h-[100dvh] bg-black bg-opacity-20 grid p-4'>
+      <aside ref={modal} className='fixed z-50 top-0 w-full h-[100dvh] grid p-4'>
+        <div onClick={() => closeModal()} className='absolute -z-10 w-full h-full bg-black bg-opacity-50 cursor-pointer'></div>
         <div className='max-w-xl w-full grid m-auto bg-undefined-icon bg-center border-bronze border-2'>
           <div className='grid-center w-full h-full bg-black bg-opacity-70'></div>
           <div className='grid-center p-4 text-white'>
