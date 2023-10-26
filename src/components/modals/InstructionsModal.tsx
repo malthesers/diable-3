d0797c6530abea29e8c0d5640477bd742bd58b62
malt9830 <malt9830@stud.kea.dk>
2023-10-26 03:12:12 +0200

    Updated instructions text

--- src/components/modals/InstructionsModal.tsx
+++ src/components/modals/InstructionsModal.tsx
@@ -7,9 +7,10 @@ export default function InstructionsModal() {
   return (
     <ModalTemplate showModal={showInstructions} closeModal={() => toggleShowInstructions(false)} title='Instructions'>
       <div className='text-lg sm:text-xl text-center space-y-2'>
-        <p>Search for an item using the input field and choose your guess.</p>
-        <p>View your past guesses to find correct and incorrect values.</p>
-        <p>A <span className='text-green-600'>green</span> background indicates a correct value, and a <span className='text-red-600'>red</span> background indicates an incorrect value.</p>
+        <p>Search for an item and enter your guess.</p>
+        <p>View guess to find correct and incorrect properties.</p>
+        <p>A <span className='text-green-600'>green</span> background indicates a correct property, and a <span className='text-red-600'>red</span> background indicates an incorrect property.</p>
+        <p>Additionally revealed properties appear in the item tooltip.</p>
         <p>Narrow down the possibilities until you find the right item!</p>
       </div>
     </ModalTemplate>
