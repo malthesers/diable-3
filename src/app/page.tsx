2872e3436d427beb9a378ed3f66f77ba1cfa4a03
malt9830 <malt9830@stud.kea.dk>
2023-10-28 01:30:10 +0200

    Surrender button only works when a game is active

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
     <main>
       <section className='max-w-[400px] sm:max-w-[430px] md:max-w-[920px] lg:max-w-none mx-auto flex flex-row flex-wrap place-content-center md:place-content-start gap-2 mb-2 px-2 lg:px-3'>
         <Button onClick={resetGame} text='New Game'/>
-        <Button onClick={surrenderGame} text='Surrender'/>
+        <Button onClick={surrenderGame} text='Surrender' className={wasGuessed ? ' brightness-50 pointer-events-none' : ''}/>
         <Button onClick={() => toggleShowInstructions(true)} text='How To'/>
       </section>
       <section className='grid md:grid-cols-2'>
--- src/components/layout/Button.tsx
+++ src/components/layout/Button.tsx
@@ -1,14 +1,14 @@
 interface ButtonProps {
   onClick: () => void,
-  classNames: string,
+  className?: string,
   text: string,
 }
 
-export default function Button({ onClick, classNames, text} : ButtonProps) {
+export default function Button({ onClick, className, text} : ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className={classNames + " w-52 aspect-[159/41] bg-[url('/images/button.png')] bg-cover bg-center text-gold uppercase duration-200 hover:text-gold-light hover:brightness-125"}
+      className={className + " w-52 aspect-[159/41] bg-[url('/images/button.png')] bg-cover bg-center text-gold uppercase duration-200 hover:text-gold-light hover:brightness-125"}
     >
       {text}
     </button>
