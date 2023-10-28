dcd15c9b4a04b1268a131798aadee432ed8ddc3e
malt9830 <malt9830@stud.kea.dk>
2023-10-28 03:09:29 +0200

    Added mightyWeapons.ts file

---
+++ src/data/1-hand/mightyWeapons.ts
@@ -0,0 +1,22 @@
+import { Item } from "@/src/interfaces/Item"
+
+interface MightyWeapon extends Item {
+  class: 'barbarian',
+  equipment: {
+    slot: '1-hand',
+    type: 'mighty weapon'
+  }
+}
+
+export const mightyWeapons:MightyWeapon[] = [
+  {
+    name: 'Fjord Cutter',
+    class: 'barbarian',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'mighty weapon'
+    },
+    legendaryPower: 'Seismic Slam attacks 50% faster and also deals [100 - 150]% increased damage against Slowed or Chilled enemies.'
+  },
+]
