ca1544510fd3fb531b10358776467b86fdc5118c
malt9830 <malt9830@stud.kea.dk>
2023-10-28 03:11:13 +0200

    Added flail.ts file

---
+++ src/data/1-hand/flail.ts
@@ -0,0 +1,22 @@
+import { Item } from "@/src/interfaces/Item"
+
+interface Flail extends Item {
+  class: 'crusader',
+  equipment: {
+    slot: '1-hand',
+    type: 'flail'
+  }
+}
+
+export const flails:Flail[] = [
+  {
+    name: 'Darklight',
+    class: 'crusader',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'flail'
+    },
+    legendaryPower: 'Fist of the Heavens now casts twice and deals [800 - 1000]% more damage.'
+  },
+]
