755bff13df5345d98dd1227f71f5781fee189809
malt9830 <malt9830@stud.kea.dk>
2023-10-28 02:52:01 +0200

    Added two handers to allItems export

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,8 @@
 import { Item } from "@/src/interfaces/Item";
 import { oneHandItems } from "./1-hand";
+import { twoHandItems } from "./2-hand";
 
 export const allItems:Item[] = [
-  ...oneHandItems
+  ...oneHandItems,
+  ...twoHandItems
 ]
