f1c171521aa126ad4fa0a94c1bb83962ef38bd4e
malt9830 <malt9830@stud.kea.dk>
2023-10-28 03:06:53 +0200

    Added wands.ts file

--- src/data/1-hand/index.ts
+++ src/data/1-hand/index.ts
@@ -1,8 +1,10 @@
 import { Item } from "@/src/interfaces/Item";
 import { handCrossbows } from "./handCrossbows";
 import { swords } from "./swords";
+import { wands } from './wands'
 
 export const oneHandItems:Item[] = [
   ...handCrossbows,
-  ...swords
+  ...swords,
+  ...wands
 ]
---
+++ src/data/1-hand/wands.ts
@@ -0,0 +1,22 @@
+import { Item } from "@/src/interfaces/Item"
+
+interface Wand extends Item {
+  class: 'wizard',
+  equipment: {
+    slot: '1-hand',
+    type: 'wand'
+  }
+}
+
+export const wands:Wand[] = [
+  {
+    name: 'Starfire',
+    class: 'wizard',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'wand'
+    },
+    legendaryPower: 'Lightning damage is increased by [10 - 15]% for every 10 yards you are from the target up to a maximum of 40 yards.'
+  },
+]
