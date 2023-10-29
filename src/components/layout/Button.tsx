d6f99226785b1280c5067b1fd339cf0d6b4ca5a4
malt9830 <malt9830@stud.kea.dk>
2023-10-29 12:15:19 +0100

    Reduced button width

--- src/components/layout/Button.tsx
+++ src/components/layout/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({ onClick, className, text} : ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className={className + " w-52 aspect-[159/41] bg-[url('/images/button.png')] bg-cover bg-center text-gold uppercase duration-200 hover:text-gold-light hover:brightness-125"}
+      className={className + " w-44 aspect-[159/41] bg-[url('/images/button.png')] bg-cover bg-center text-gold uppercase duration-200 hover:text-gold-light hover:brightness-125"}
     >
       {text}
     </button>
