5603a92d9b377ecd9c71e4ab1d7735ce99356423
malt9830 <malt9830@stud.kea.dk>
2023-10-30 00:57:42 +0100

    Added ItemSearch component

---
+++ src/components/game/search/ItemSearch.tsx
@@ -0,0 +1,31 @@
+import { CSSTransition } from 'react-transition-group'
+import { Item } from '@/src/interfaces/Item'
+import { useRef } from 'react'
+import ItemNameplate from '../ItemNameplate'
+
+interface ItemSearchProps {
+  submitGuess: (item:Item) => void,
+  item: Item
+}
+
+export default function ItemSearch({ submitGuess, item }: ItemSearchProps) {
+  const node = useRef(null)
+
+  return (
+    <CSSTransition
+      timeout={250}
+      classNames='search'
+      nodeRef={node}
+    >
+      <article
+        ref={node}
+        key={item.name} tabIndex={0}
+        onClick={() => submitGuess(item)}
+        onKeyDown={(e) => {if (e.key === 'Enter') submitGuess(item)}}
+        className='group focus-within:outline-none'
+      >
+        <ItemNameplate item={item} hover={true}/>
+      </article>
+    </CSSTransition>
+  )
+}
--- src/components/game/search/SearchResults.tsx
+++ src/components/game/search/SearchResults.tsx
@@ -1,8 +1,7 @@
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import { TransitionGroup } from 'react-transition-group'
 import { useItems } from '@/src/context/ItemsProvider'
 import { Item } from '@/src/interfaces/Item'
-import ItemNameplate from '../ItemNameplate'
-
+import ItemSearch from './ItemSearch'
 
 interface SearchResultsProps {
   submitGuess: (item:Item) => void,
@@ -14,24 +13,13 @@ export default function SearchResults({ submitGuess, results }: SearchResultsPro
 
   return (
     <div className='absolute z-10 w-full h-fit mt-2'>
-      {/* <TransitionGroup className='w-fit flex flex-col gap-2'> */}
-        { 1 < search.length && results.map((item) =>
-          <CSSTransition
-            key={item.name}
-            timeout={250}
-            classNames='search'
-          >
-            <article
-              key={item.name} tabIndex={0}
-              onClick={() => submitGuess(item)}
-              onKeyDown={(e) => {if (e.key === 'Enter') submitGuess(item)}}
-              className='group focus-within:outline-none'
-            >
-              <ItemNameplate item={item} hover={true}/>
-            </article>
-          </CSSTransition>
-        )}
-      {/* </TransitionGroup> */}
+      { 1 < search.length && 
+        <TransitionGroup className='w-fit flex flex-col gap-2'>
+          {results.map((item) =>(
+            <ItemSearch key={item.name} item={item} submitGuess={submitGuess}/>
+          ))}
+        </TransitionGroup>
+      }
     </div>
   )
 }
