941aefe5e82a5efd72bfa59bb233358469d17d37
malt9830 <malt9830@stud.kea.dk>
2023-10-29 02:05:30 +0100

    Replaced logical and with ternary in ItemNameplate

--- src/components/game/ItemNameplate.tsx
+++ src/components/game/ItemNameplate.tsx
@@ -11,7 +11,7 @@ export default function ItemNameplate({ item, hover = false }: ItemNameplateProp
   const textColor = item.name === 'unidentified' ? 'text-undefined' : `text-${item.quality}`
 
   return (
-    <p className={(hover && hoverClasses) + ` bg-${item.quality}-name` + ' nameplate mx-auto aspect-[350/40] bg-no-repeat bg-cover bg-center grid duration-500'}>
+    <p className={(hover ? hoverClasses : '') + ` bg-${item.quality}-name` + ' nameplate mx-auto aspect-[350/40] bg-no-repeat bg-cover bg-center grid duration-500'}>
       <KnownValue value={item.name} className={textColor + ' text-sm/4 sm:text-base/4 md:text-lg/4 px-6 sm:px-12 text-center leading-none uppercase place-self-center duration-500'}/>
     </p>
   )
