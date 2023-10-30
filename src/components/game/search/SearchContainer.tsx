a178a4d7d7548e770a9712ab86a20f5c44828fd6
malt9830 <malt9830@stud.kea.dk>
2023-10-30 02:15:26 +0100

    Fixed remaining array after ref changes

--- src/components/game/search/SearchContainer.tsx
+++ src/components/game/search/SearchContainer.tsx
@@ -7,7 +7,7 @@ import SearchBar from './SearchBar'
 export default function SearchContainer() {
   const { items, guesses, validateGuess, search, setSearch } = useItems()
   const ref = useRef<HTMLInputElement | null>(null)
-  const remaining:Item[] = items.filter(item => !guesses.includes(item))
+  const remaining:Item[] = items.filter(item =>  !guesses.some(guess => guess.name === item.name))
   const results:Item[] = remaining.filter(item => item.name.toLowerCase().includes(search.toLowerCase())).slice(0, 10)
 
   function submitGuess(item:Item) {
