70ae9833543b5435b6f329ad4c1c7092af7f08a5
malt9830 <malt9830@stud.kea.dk>
2023-10-30 13:04:01 +0100

    QualitiesModal now starts new game on close
    - On modal closing instead of any chosen qualities change

--- src/components/modals/QualitiesModal.tsx
+++ src/components/modals/QualitiesModal.tsx
@@ -6,21 +6,21 @@ export default function QualitiesModal() {
   const { showQualities, toggleShowQualities } = useModals()
   const { chosen, toggleChosen, resetGame } = useItems()
 
-  function updateChosen(quality: keyof typeof chosen) {
-    toggleChosen(quality)
+  function closeModal() {
+    toggleShowQualities(false)
     resetGame()
   }
 
   return (
     <ModalTemplate
       showModal={showQualities}
-      closeModal={() => toggleShowQualities(false)}
+      closeModal={() => closeModal()}
       title='Qualities'
     >
       <p className='text-xl text-center'>Below you can toggle item qualities, dictating which items will be <span className='text-green-600'>included</span> and <span className='text-red-800'>excluded</span> in the list of possible answers.</p>
       <div className='grid grid-cols-2 sm:grid-cols-3 gap-4 mt-2'>
         { Object.entries(chosen).map(([quality, active]) => 
-          <div onClick={() => updateChosen(quality as keyof typeof chosen)} key={quality} className={'grid bg-undefined-icon text-center cursor-pointer duration-200 hover:shadow-quality-inner'}>
+          <div onClick={() => toggleChosen(quality as keyof typeof chosen)} key={quality} className={'grid bg-undefined-icon text-center cursor-pointer duration-200 hover:shadow-quality-inner'}>
             <div className={(active ? 'bg-green-600' : 'bg-red-800') + ' bg-opacity-40 grid-center duration-300'}></div>
             <p className='uppercase p-4 grid-center'>{quality}</p>
           </div>
