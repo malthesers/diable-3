5cac81c84b4dbf896481d54fcea1167f286058e9
malt9830 <malt9830@stud.kea.dk>
2023-10-30 01:38:37 +0100

    Fixed transition error for SearchResults

--- src/components/game/search/SearchResults.tsx
+++ src/components/game/search/SearchResults.tsx
@@ -2,25 +2,40 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import { useItems } from '@/src/context/ItemsProvider'
 import { Item } from '@/src/interfaces/Item'
 import ItemSearch from './ItemSearch'
+import { RefObject, createRef } from 'react'
 
 interface SearchResultsProps {
   submitGuess: (item:Item) => void,
   results: Item[],
 }
 
+interface ItemRef extends Item {
+  ref: RefObject<HTMLDivElement>
+}
+
 export default function SearchResults({ submitGuess, results }: SearchResultsProps) {
   const { search } = useItems()
+
+  const resultsRef:ItemRef[] = results.map((item) => {
+    return {
+      ...item,
+      ref: createRef()
+    }
+  })
 
   return (
     <div className='absolute z-10 w-full h-fit mt-2'>
       <TransitionGroup className='w-fit flex flex-col gap-2'>
-        {1 < search.length && results.map((item) => (
+        {1 < search.length && resultsRef.map((item) => (
           <CSSTransition
             timeout={250}
             classNames='search'
             key={item.name}
+            nodeRef={item.ref}
           >
-            <ItemSearch submitGuess={submitGuess} item={item} />
+            <div ref={item.ref}>
+              <ItemSearch submitGuess={submitGuess} item={item}/>
+            </div>
           </CSSTransition>
         ))}
       </TransitionGroup>
