105e14198f2b62633d9ab389c619a78678f25d97
malt9830 <malt9830@stud.kea.dk>
2023-10-30 01:53:00 +0100

    Removed refs from ItemSearch

--- src/components/game/search/ItemSearch.tsx
+++ src/components/game/search/ItemSearch.tsx
@@ -1,5 +1,4 @@
 import { Item } from '@/src/interfaces/Item'
-import { useRef } from 'react'
 import ItemNameplate from '../ItemNameplate'
 
 interface ItemSearchProps {
@@ -8,11 +7,8 @@ interface ItemSearchProps {
 }
 
 export default function ItemSearch({ submitGuess, item }: ItemSearchProps) {
-  const node = useRef(null)
-
   return (
     <article
-      ref={node}
       key={item.name} tabIndex={0}
       onClick={() => submitGuess(item)}
       onKeyDown={(e) => {if (e.key === 'Enter') submitGuess(item)}}
