7d8e3f2f54b533e80dafd18f77341df4f79327de
malt9830 <malt9830@stud.kea.dk>
2023-10-31 04:52:58 +0100

    Added new game information to QualitiesModal

--- src/components/modals/QualitiesModal.tsx
+++ src/components/modals/QualitiesModal.tsx
@@ -32,6 +32,7 @@ export default function QualitiesModal() {
       title='Qualities'
     >
       <p className='text-base md:text-xl text-center'>Below you can toggle item qualities, dictating which items will be <span className='text-green-600'>included</span> and <span className='text-red-800'>excluded</span> in the list of possible answers.</p>
+      <p className='text-base md:text-xl text-center mt-2'>Closing the pop-up will start a new game, if any changes to qualities were made.</p>
       <div className='grid 2xs:grid-cols-2 sm:grid-cols-3 gap-2 xs:gap-4 mt-2'>
         { Object.entries(chosen).map(([quality, active]) => 
           <div onClick={() => toggleChosen(quality as keyof typeof chosen)} key={quality} className={'grid bg-undefined-icon text-center cursor-pointer shadow-item-inner duration-200 hover:shadow-quality-inner'}>
