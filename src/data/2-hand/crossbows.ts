8b0ba4db70370998a51bb18bb63885fdc5f35933
malt9830 <malt9830@stud.kea.dk>
2023-11-01 15:50:00 +0100

    Added crossbows export

---
+++ src/data/2-hand/crossbows.ts
@@ -0,0 +1,23 @@
+import { Item } from '@/src/interfaces/Item'
+
+interface Crossbow extends Item {
+  class: 'generic',
+  equipment: {
+    slot: '2-hand',
+    type: 'crossbow'
+  }
+}
+
+export const crossbows:Crossbow[] = [
+  {
+    name: 'Demon Machine',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'crossbow'
+    },
+    legendaryPower: '[35 - 65]% chance to shoot explosive bolts when attacking.',
+    elementalDamage: 'fire'
+  },
+]
