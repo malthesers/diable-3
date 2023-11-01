7c13e9cc9c74231c085fb3a1df09bb2ede91e958
malt9830 <malt9830@stud.kea.dk>
2023-11-01 15:51:47 +0100

    Added 2h axes export

---
+++ src/data/2-hand/axes.ts
@@ -0,0 +1,22 @@
+import { Item } from '@/src/interfaces/Item'
+
+interface Axe extends Item {
+  class: 'generic',
+  equipment: {
+    slot: '2-hand',
+    type: 'axe'
+  }
+}
+
+export const axes:Axe[] = [
+  {
+    name: 'The Executioner',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'axe'
+    },
+    legendaryPower: 'Attacks will slay enemies with less than [5 - 10]% health.',
+  },
+]
--- src/data/2-hand/index.ts
+++ src/data/2-hand/index.ts
@@ -1,6 +1,10 @@
 import { Item } from '@/src/interfaces/Item';
+import { crossbows } from './crossbows';
 import { bows } from './bows';
+import { axes } from './axes';
 
 export const twoHandItems:Item[] = [
+  ...crossbows,
+  ...axes,
   ...bows,
 ]
