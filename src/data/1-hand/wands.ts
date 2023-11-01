e45e3ad145257aea10bd8da12b43958c370b5bb5
malt9830 <malt9830@stud.kea.dk>
2023-11-01 15:45:34 +0100

    Added elemental damage to Starfire

--- src/data/1-hand/wands.ts
+++ src/data/1-hand/wands.ts
@@ -17,6 +17,7 @@ export const wands:Wand[] = [
       slot: '1-hand',
       type: 'wand'
     },
-    legendaryPower: 'Lightning damage is increased by [10 - 15]% for every 10 yards you are from the target up to a maximum of 40 yards.'
+    legendaryPower: 'Lightning damage is increased by [10 - 15]% for every 10 yards you are from the target up to a maximum of 40 yards.',
+    elementalDamage: 'cold'
   },
 ]
