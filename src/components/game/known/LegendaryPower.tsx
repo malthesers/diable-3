e2d9949d63e00b1a014ddf64d63251ef0d54e096
malt9830 <malt9830@stud.kea.dk>
2023-11-01 16:20:53 +0100

    Re-factored LegendaryPower

--- src/components/game/known/LegendaryPower.tsx
+++ src/components/game/known/LegendaryPower.tsx
@@ -6,21 +6,15 @@ import { useRef } from 'react'
 
 export default function LegendaryPower({ power }: { power: string | undefined }) {
   const { answer, known, guesses } = useItems()
-  const wasGuessed = answer?.name === known.name
-  const showHint = guesses.length < 1
   const ref = useRef<HTMLDivElement>(null)
-  
-  let powerMan = power
-
-  if (showHint && !wasGuessed) {
-    powerMan = ''
-  }
+  const wasGuessed = answer?.name === known.name
+  const legendaryPower = (guesses.length < 9 && !wasGuessed) ? '' : power
 
   return (
     <SwitchTransition mode='out-in'>
       <CSSTransition
         classNames='fade'
-        key={powerMan}
+        key={legendaryPower}
         nodeRef={ref}
         addEndListener={(done: () => void) =>
           ref.current?.addEventListener('transitionend', done, false)
@@ -28,12 +22,12 @@ export default function LegendaryPower({ power }: { power: string | undefined })
       >
         <div ref={ref}>
           <p className={(wasGuessed ? 'blur-none' : 'blur-sm hover:blur-none') + ' duration-200'}>
-            { powerMan && <Image
+            { legendaryPower && <Image
               src={Bullet}
               alt='Legendary power bullet'
               className='inline-block w-4 h-4 mb-1 mr-1'
             />}
-            <span className='text-legendary normal-case'>{powerMan}</span>
+            <span className='text-legendary normal-case'>{legendaryPower}</span>
           </p>
         </div>
       </CSSTransition>
