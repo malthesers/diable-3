eb458947fe571a66883534ca83b95f8ad60c7ee4
malt9830 <malt9830@stud.kea.dk>
2023-11-01 15:52:54 +0100

    Added 2h maces export

--- src/data/2-hand/index.ts
+++ src/data/2-hand/index.ts
@@ -1,10 +1,12 @@
 import { Item } from '@/src/interfaces/Item';
 import { crossbows } from './crossbows';
+import { maces } from './maces';
 import { bows } from './bows';
 import { axes } from './axes';
 
 export const twoHandItems:Item[] = [
   ...crossbows,
+  ...maces,
   ...axes,
   ...bows,
 ]
