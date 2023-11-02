78bb6a9b5476aab7729165720b67c91e921d0f51
malt9830 <malt9830@stud.kea.dk>
2023-11-02 08:53:42 +0100

    Added legendary + common 2h axes

--- src/data/2-hand/axes.ts
+++ src/data/2-hand/axes.ts
@@ -10,6 +10,37 @@ interface Axe extends Item {
 
 export const axes:Axe[] = [
   {
+    name: 'Burst of Wrath',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'axe'
+    },
+    legendaryPower: 'Killing enemies and destroying objects has a chance to grant 20% of your maximum primary resource.',
+  },
+  {
+    name: 'Butcher\'s Carver',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'axe'
+    },
+    legendaryPower: 'The Butcher still inhabits his carver.',
+  },
+  {
+    name: 'Cinder Switch',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'axe'
+    },
+    legendaryPower: '[25 - 50%] chance to cast a fireball when attacking.',
+    elementalDamage: 'fire'
+  },
+  {
     name: 'The Executioner',
     class: 'generic',
     quality: 'legendary',
@@ -18,5 +49,25 @@ export const axes:Axe[] = [
       type: 'axe'
     },
     legendaryPower: 'Attacks will slay enemies with less than [5 - 10%] health.',
+  },
+  {
+    name: 'Messerschmidt\'s Reaver',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'axe'
+    },
+    legendaryPower: 'Reduce the remaining cooldown of one of your skills by [1] second when you slay an enemy.',
+    elementalDamage: 'fire'
+  },
+  {
+    name: 'Skorn',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'axe'
+    },
   },
 ]
