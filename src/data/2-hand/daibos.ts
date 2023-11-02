8eb5147ee71c5ab0c0833700b924b0b504049c3c
malt9830 <malt9830@stud.kea.dk>
2023-11-02 09:09:01 +0100

    Added legendary + set daibos

--- src/data/2-hand/daibos.ts
+++ src/data/2-hand/daibos.ts
@@ -10,6 +10,16 @@ interface Daibo extends Item {
 
 export const daibos:Daibo[] = [
   {
+    name: 'Flying Dragon',
+    class: 'monk',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'daibo'
+    },
+    legendaryPower: 'Chance to double your attack speed when attacking.',
+  },
+  {
     name: 'Balance',
     class: 'monk',
     quality: 'legendary',
@@ -19,5 +29,83 @@ export const daibos:Daibo[] = [
     },
     legendaryPower: 'The damage of Tempest Rush is increased by [450 - 600%] and when your Tempest Rush hits 3 or fewer enemies, it gains 100% Critical Hit Chance.',
     elementalDamage: 'holy'
+  },
+  {
+    name: 'Incense Torch of the Grand Temple',
+    class: 'monk',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'daibo'
+    },
+    legendaryPower: 'Reduces the Spirit cost of Wave of Light by 50% and increases its damage by [450 - 550%].',
+  },
+  {
+    name: 'Lai Yui\'s Persuader',
+    class: 'monk',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'daibo'
+    },
+    elementalDamage: 'cold'
+  },
+  {
+    name: 'Rozpedin\'s Force',
+    class: 'monk',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'daibo'
+    },
+    elementalDamage: 'holy'
+  },
+  {
+    name: 'Staff of Kyro',
+    class: 'monk',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'daibo'
+    },
+  },
+  {
+    name: 'The Flow of Eternity',
+    class: 'monk',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'daibo'
+    },
+    legendaryPower: 'Increase the damage of Seven-Sided Strike by 100% and reduce the cooldown of Seven-Sided Strike by [45 - 60%].'
+  },
+  {
+    name: 'The Paddle',
+    class: 'monk',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'daibo'
+    },
+    legendaryPower: 'Slap!'
+  },
+  {
+    name: 'Warstaff of General Quang',
+    class: 'monk',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'daibo'
+    },
+    legendaryPower: 'Tempest Rush gains the effect of the Tailwind rune.'
+  },
+  {
+    name: 'Inna\'s Reach',
+    class: 'monk',
+    quality: 'set',
+    equipment: {
+      slot: '2-hand',
+      type: 'daibo'
+    },
   },
 ]
