da71accef638714e8ff9d2e8915eecf3175f81dd
malt9830 <malt9830@stud.kea.dk>
2023-11-02 07:22:11 +0100

    Added legendary + set 1h mighty weapons

--- src/data/1-hand/mightyWeapons.ts
+++ src/data/1-hand/mightyWeapons.ts
@@ -10,6 +10,37 @@ interface MightyWeapon extends Item {
 
 export const mightyWeapons:MightyWeapon[] = [
   {
+    name: 'Ambo\'s Pride',
+    class: 'barbarian',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'mighty weapon'
+    },
+    legendaryPower: 'Attacking with Whirlwind also applies Rend, and the total damage of Rend is dealt over 1 second.',
+  },
+  {
+    name: 'Blade of the Warlord',
+    class: 'barbarian',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'mighty weapon'
+    },
+    legendaryPower: 'Bash consumes up to 40 Fury to deal up to [400 - 500]% increased damage.',
+    elementalDamage: 'holy'
+  },
+  {
+    name: 'Dishonored Legacy',
+    class: 'barbarian',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'mighty weapon'
+    },
+    legendaryPower: 'Cleave deals up to [300 - 400]% increased damage based on percentage of missing Fury.',
+  },
+  {
     name: 'Fjord Cutter',
     class: 'barbarian',
     quality: 'legendary',
@@ -19,5 +50,65 @@ export const mightyWeapons:MightyWeapon[] = [
     },
     legendaryPower: 'Seismic Slam attacks 50% faster and also deals [100 - 150]% increased damage against Slowed or Chilled enemies.',
     elementalDamage: 'cold'
+  },
+  {
+    name: 'Night\'s Reaping',
+    class: 'barbarian',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'mighty weapon'
+    },
+    elementalDamage: 'cold'
+  },
+  {
+    name: 'Oathkeeper',
+    class: 'barbarian',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'mighty weapon'
+    },
+    legendaryPower: 'Your primary skills attack 50% faster and deal [150 - 200]% increased damage.'
+  },
+  {
+    name: 'Remorseless',
+    class: 'barbarian',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'mighty weapon'
+    },
+    legendaryPower: 'While both Wrath of the Berserker and Call of the Ancients are active, Hammer of the Ancients deals [200 - 800]% increased damage.'
+  },
+  {
+    name: 'Bul-Katho\'s Solemn Vow',
+    class: 'barbarian',
+    quality: 'set',
+    equipment: {
+      slot: '1-hand',
+      type: 'mighty weapon'
+    },
+    elementalDamage: 'holy'
+  },
+  {
+    name: 'Bul-Katho\'s Warrior Blood',
+    class: 'barbarian',
+    quality: 'set',
+    equipment: {
+      slot: '1-hand',
+      type: 'mighty weapon'
+    },
+    elementalDamage: 'holy'
+  },
+  {
+    name: 'Hallowed Nemesis',
+    class: 'barbarian',
+    quality: 'set',
+    equipment: {
+      slot: '1-hand',
+      type: 'mighty weapon'
+    },
+    elementalDamage: 'holy'
   },
 ]
