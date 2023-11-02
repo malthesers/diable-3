1550badc1feee4864ee6fb2bd78d0abf99f2c629
malt9830 <malt9830@stud.kea.dk>
2023-11-02 10:27:53 +0100

    Added crusader shields

--- src/data/off-hand/crusaderShields.ts
+++ src/data/off-hand/crusaderShields.ts
@@ -10,9 +10,129 @@ interface CrusaderShield extends Item {
 
 export const crusaderShields:CrusaderShield[] = [
   {
-    name: '',
+    name: 'Akarat\'s Awakening',
     class: 'crusader',
     quality: 'legendary',
+    equipment: {
+      slot: 'off-hand',
+      type: 'crusader shield'
+    },
+    legendaryPower: 'Every successful block has a [20 - 25%] chance to reduce all cooldowns by 1 second.'
+  },
+  {
+    name: 'Frydehr\'s Wrath',
+    class: 'crusader',
+    quality: 'legendary',
+    equipment: {
+      slot: 'off-hand',
+      type: 'crusader shield'
+    },
+    legendaryPower: 'Condemn has no cooldown and has its damage increased by [600 - 800%], but instead costs 40 Wrath.'
+  },
+  {
+    name: 'Guard of Johanna',
+    class: 'crusader',
+    quality: 'legendary',
+    equipment: {
+      slot: 'off-hand',
+      type: 'crusader shield'
+    },
+    legendaryPower: 'Blessed Hammer damage is increased by [200 - 250%] for the first 3 enemies it hits.'
+  },
+  {
+    name: 'Hallowed Bulwark',
+    class: 'crusader',
+    quality: 'legendary',
+    equipment: {
+      slot: 'off-hand',
+      type: 'crusader shield'
+    },
+    legendaryPower: 'Iron Skin also increases your Block Amount by [45 - 60%].'
+  },
+  {
+    name: 'Hellskull',
+    class: 'crusader',
+    quality: 'legendary',
+    equipment: {
+      slot: 'off-hand',
+      type: 'crusader shield'
+    },
+    legendaryPower: 'Gain 10% increased damage while wielding a two-handed weapon.'
+  },
+  {
+    name: 'Jekangbord',
+    class: 'crusader',
+    quality: 'legendary',
+    equipment: {
+      slot: 'off-hand',
+      type: 'crusader shield'
+    },
+    legendaryPower: 'Blessed Shield ricochets to 6 additional enemies and has its damage increased by [300 - 400%].'
+  },
+  {
+    name: 'Piro Marella',
+    class: 'crusader',
+    quality: 'legendary',
+    equipment: {
+      slot: 'off-hand',
+      type: 'crusader shield'
+    },
+    legendaryPower: 'Reduces the Wrath cost of Shield Bash by [40 - 50%].'
+  },
+  {
+    name: 'Salvation',
+    class: 'crusader',
+    quality: 'legendary',
+    equipment: {
+      slot: 'off-hand',
+      type: 'crusader shield'
+    },
+    legendaryPower: 'Blocked attacks heal you and your allies for [20 - 30%] of the amount blocked.'
+  },
+  {
+    name: 'Shield of Fury',
+    class: 'crusader',
+    quality: 'legendary',
+    equipment: {
+      slot: 'off-hand',
+      type: 'crusader shield'
+    },
+    legendaryPower: 'Each time an enemy takes damage from your Heaven\'s Fury, it increases the damage they take from your Heaven\'s Fury by [25 - 30%]. This effect stacks up to 10 times.'
+  },
+  {
+    name: 'Sublime Conviction',
+    class: 'crusader',
+    quality: 'legendary',
+    equipment: {
+      slot: 'off-hand',
+      type: 'crusader shield'
+    },
+    legendaryPower: 'When you block, you have up to a [15 - 20%] chance to Stun the attacker for 1.5 seconds based on your current Wrath.'
+  },
+  {
+    name: 'The Final Witness',
+    class: 'crusader',
+    quality: 'legendary',
+    equipment: {
+      slot: 'off-hand',
+      type: 'crusader shield'
+    },
+    legendaryPower: 'Shield Glare now hits all enemies around you.'
+  },
+  {
+    name: 'Unrelenting Phalanx',
+    class: 'crusader',
+    quality: 'legendary',
+    equipment: {
+      slot: 'off-hand',
+      type: 'crusader shield'
+    },
+    legendaryPower: 'Phalanx now casts twice.'
+  },
+  {
+    name: 'Shield of the Steed',
+    class: 'crusader',
+    quality: 'set',
     equipment: {
       slot: 'off-hand',
       type: 'crusader shield'
