72cb151ee9d3b8da6055682ba6ca520c6da620d2
malt9830 <malt9830@stud.kea.dk>
2023-11-02 05:27:13 +0100

    Added legendary + set 1h axes

--- src/data/1-hand/axes.ts
+++ src/data/1-hand/axes.ts
@@ -10,6 +10,36 @@ interface Axe extends Item {
 
 export const axes:Axe[] = [
   {
+    name: 'The Burning Axe of Sankis',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'axe'
+    },
+    legendaryPower: 'Chance to fight through the pain when enemies hit you.',
+    elementalDamage: 'fire'
+  },
+  {
+    name: 'The Butcher\'s Sickle',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'axe'
+    },
+    legendaryPower: '[20 - 25]% chance to drag enemies to you when attacking.',
+  },
+  {
+    name: 'Flesh Tearer',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'axe'
+    },
+  },
+  {
     name: 'Genzaniku',
     class: 'generic',
     quality: 'legendary',
@@ -18,5 +48,56 @@ export const axes:Axe[] = [
       type: 'axe'
     },
     legendaryPower: 'Chance to summon a ghostly Fallen Champion when attacking.'
+  },
+  {
+    name: 'Hack',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'axe'
+    },
+    legendaryPower: '[75 - 100]% of your Thorns damage is applied on every attack.'
+  },
+  {
+    name: 'Sky Splitter',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'axe'
+    },
+    legendaryPower: '[15 - 20]% chance to Smite enemies for 600-750% weapon damage as Lightning when you hit them.',
+    elementalDamage: 'holy'
+  },
+  {
+    name: 'Utar\'s Roar',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'axe'
+    },
+    elementalDamage: 'cold'
+  },
+  {
+    name: 'Mordullu\'s Promise',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'axe'
+    },
+    legendaryPower: 'Firebomb generates [100 - 125] Mana.',
+  },
+  {
+    name: 'Hallowed Breach',
+    class: 'generic',
+    quality: 'set',
+    equipment: {
+      slot: '1-hand',
+      type: 'axe'
+    },
+    elementalDamage: 'holy'
   },
 ]
