e6b9f6f163e9d6ba459750a15ed7ced9d6539359
malt9830 <malt9830@stud.kea.dk>
2023-11-02 07:37:05 +0100

    Added legendary + set 1h scythes

--- src/data/1-hand/scythes.ts
+++ src/data/1-hand/scythes.ts
@@ -10,6 +10,26 @@ interface Scythe extends Item {
 
 export const scythes:Scythe[] = [
   {
+    name: 'Funerary Pick',
+    class: 'necromancer',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'scythe'
+    },
+    legendaryPower: 'Siphon Blood drains blood from 2 additional targets. Each target takes [250 - 300]% increased damage from you unless there is only 1 target, then it gains double the increased damage. The bonus from Siphon Blood - Power Shift is now 20% per stack and benefits all skills.'
+  },
+  {
+    name: 'Scythe of the Cycle',
+    class: 'necromancer',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'scythe'
+    },
+    legendaryPower: 'Your Secondary skills deal [350 - 400]% additional damage while Bone Armor is active but reduce the remaining duration of Bone Armor by 4 seconds. '
+  },
+  {
     name: 'Trag\'Oul\'s Corroded Fang',
     class: 'necromancer',
     quality: 'legendary',
@@ -18,5 +38,14 @@ export const scythes:Scythe[] = [
       type: 'scythe'
     },
     legendaryPower: 'The Cursed Scythe rune for Grim Scythe now has a 100% chance to apply a curse and you deal [150 - 200]% increased damage to cursed enemies.'
+  },
+  {
+    name: 'Jesseth Skullscythe',
+    class: 'necromancer',
+    quality: 'set',
+    equipment: {
+      slot: '1-hand',
+      type: 'scythe'
+    },
   },
 ]
