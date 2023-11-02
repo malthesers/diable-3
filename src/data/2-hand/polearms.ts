3ffd3a962b5bff7c7ef8c6dbdafabf85a810d721
malt9830 <malt9830@stud.kea.dk>
2023-11-02 09:30:26 +0100

    Added legendary + common polearms

--- src/data/2-hand/polearms.ts
+++ src/data/2-hand/polearms.ts
@@ -10,6 +10,26 @@ interface Polearm extends Item {
 
 export const polearms:Polearm[] = [
   {
+    name: 'Bovine Bardiche',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'polearm'
+    },
+    legendaryPower: 'Chance on hit to summon a herd of murderous cows.',
+    elementalDamage: 'lightning'
+  },
+  {
+    name: 'Heart Slaugther',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'polearm'
+    },
+  },
+  {
     name: 'Pledge of Caldeum',
     class: 'generic',
     quality: 'legendary',
@@ -18,5 +38,36 @@ export const polearms:Polearm[] = [
       type: 'polearm'
     },
     elementalDamage: 'fire'
+  },
+  {
+    name: 'Standoff',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'polearm'
+    },
+    legendaryPower: 'Furious Charge deals increased damage equal to [400 - 500%]% of your bonus movement speed.',
+    elementalDamage: 'cold'
+  },
+  {
+    name: 'Vigilance',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'polearm'
+    },
+    legendaryPower: 'Getting hit has a chance to automatically cast Inner Sanctuary..',
+    elementalDamage: 'holy'
+  },
+  {
+    name: 'Man Prodder',
+    class: 'generic',
+    quality: 'common',
+    equipment: {
+      slot: '2-hand',
+      type: 'polearm'
+    },
   },
 ]
