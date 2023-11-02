3c890c08a3e3fe6477b4e1a599544d011a5985d2
malt9830 <malt9830@stud.kea.dk>
2023-11-02 09:33:02 +0100

    Added legendary + common 2h scythes

--- src/data/2-hand/scythes.ts
+++ src/data/2-hand/scythes.ts
@@ -10,6 +10,36 @@ interface Scythe extends Item {
 
 export const scythes:Scythe[] = [
   {
+    name: 'Bloodtide Blade',
+    class: 'necromancer',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'scythe'
+    },
+    legendaryPower: 'Death Nova deals [300 - 400%] increased damage for every enemy within 25 yards, up to 25 enemies.',
+  },
+  {
+    name: 'Bloodtide Blade',
+    class: 'necromancer',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'scythe'
+    },
+    legendaryPower: 'Bone Spear now costs 40 Essence and deals [550 - 700%] increased damage.',
+  },
+  {
+    name: 'Nayr\'s Black Death',
+    class: 'necromancer',
+    quality: 'legendary',
+    equipment: {
+      slot: '2-hand',
+      type: 'scythe'
+    },
+    legendaryPower: 'Each different poison skill you use increases the damage of your poison skills by [75 - 100%] for 15 seconds.',
+  },
+  {
     name: 'Reilena\'s Shadowhook',
     class: 'necromancer',
     quality: 'legendary',
@@ -18,5 +48,14 @@ export const scythes:Scythe[] = [
       type: 'scythe'
     },
     legendaryPower: 'Every point of Maximum Essence increases your damage by 0.5% and Bone Spikes generates [2 - 5] additional Essence for each enemy hit.',
+  },
+  {
+    name: 'The Reaper\'s Kiss',
+    class: 'necromancer',
+    quality: 'common',
+    equipment: {
+      slot: '2-hand',
+      type: 'scythe'
+    },
   },
 ]
