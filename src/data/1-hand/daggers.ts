19da9f93dd9fc089991356193c5503df9bee6d31
malt9830 <malt9830@stud.kea.dk>
2023-11-02 05:54:13 +0100

    Added legendary daggers

--- src/data/1-hand/daggers.ts
+++ src/data/1-hand/daggers.ts
@@ -10,6 +10,16 @@ interface Dagger extends Item {
 
 export const daggers:Dagger[] = [
   {
+    name: 'Blood-Magic Edge',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'dagger'
+    },
+    legendaryPower: 'Blood oozes from you.',
+  },
+  {
     name: 'Envious Blade',
     class: 'generic',
     quality: 'legendary',
@@ -19,5 +29,67 @@ export const daggers:Dagger[] = [
     },
     legendaryPower: 'Gain 100% Critical Hit Chance against enemies at full health.',
     elementalDamage: 'poison'
+  },
+  {
+    name: 'Eun-jang-do',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'dagger'
+    },
+    legendaryPower: 'Attacking enemies below [17 - 20]% Life freezes them for 3 seconds.',
+    elementalDamage: 'cold'
+  },
+  {
+    name: 'Karlei\'s Point',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'dagger'
+    },
+    legendaryPower: 'The damage of Impale is increased by [300 - 375]% and it returns 15 Hatred if it hits an enemy already Impaled.',
+    elementalDamage: 'cold'
+  },
+  {
+    name: 'Lord Greenstone\'s Fan',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'dagger'
+    },
+    legendaryPower: 'Every second, gain [300 - 400]% increased damage for your next Fan of Knives. Stacks up to 30 times.',
+    elementalDamage: 'cold'
+  },
+  {
+    name: 'Pig Sticker',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'dagger'
+    },
+    legendaryPower: 'Squeal!',
+  },
+  {
+    name: 'The Horadric Hamburger',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'dagger'
+    },
+  },
+  {
+    name: 'Wizardspike',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: '1-hand',
+      type: 'dagger'
+    },
+    legendaryPower: 'Performing an attack has a 25% chance to hurl a Frozen Orb. Arcane Orb deals [300 - 350]% increased damage.'
   },
 ]
