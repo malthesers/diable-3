133bb41f1147147eca80e8822a55a7d9f7e6f114
malt9830 <malt9830@stud.kea.dk>
2023-11-03 14:54:54 +0100

    Added legendary + common shoulders

---
+++ src/data/shoulders/index.ts
@@ -0,0 +1,131 @@
+import { Item } from '@/src/interfaces/Item';
+
+interface Shoulders extends Item {
+  class: 'generic',
+  equipment: {
+    slot: 'shoulders',
+    type: 'shoulders'
+  }
+}
+
+export const shouldersItems:Shoulders[] = [
+  {
+    name: 'Corpsewhisper Pauldrons',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'shoulders',
+      type: 'shoulders'
+    },
+    legendaryPower: 'Corpse Lance damage is increased by [25 - 30%] for 3 seconds when you consume a corpse. Max 20 stacks.'
+  },
+  {
+    name: 'Corruption',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'shoulders',
+      type: 'shoulders'
+    },
+  },
+  {
+    name: 'Death Watch Mantle',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'shoulders',
+      type: 'shoulders'
+    },
+    legendaryPower: '[25 - 30%] chance to explode in a fan of knives for 750-950% weapon damage when hit.'
+  },
+  {
+    name: 'Fury of the Ancients',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'shoulders',
+      type: 'shoulders'
+    },
+    legendaryPower: 'Call of the Ancients gains the effect of the Ancients\' Fury rune, and your Ancients attack 100% faster.'
+  },
+  {
+    name: 'Homing Pads',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'shoulders',
+      type: 'shoulders'
+    },
+    legendaryPower: 'Your Town Portal is no longer interrupted by taking damage. While casting Town Portal you gain a protective bubble that reduces damage taken by [50 - 65%]. (Emanates)'
+  },
+  {
+    name: 'Lefebvre\'s Soliloquy',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'shoulders',
+      type: 'shoulders'
+    },
+    legendaryPower: 'Cyclone Strike reduces your damage taken by [40 - 50%] for 5 seconds.'
+  },
+  {
+    name: 'Mantle of Channeling',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'shoulders',
+      type: 'shoulders'
+    },
+    legendaryPower: 'While channeling Siphon Blood, Whirlwind, Rapid Fire, Strafe, Tempest Rush, Firebats, Arcane Torrent, Disintegrate, or Ray of Frost, you deal [20 - 25%] increased damage and take 25% reduced damage.'
+  },
+  {
+    name: 'Pauldrons of the Skeleton King',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'shoulders',
+      type: 'shoulders'
+    },
+    legendaryPower: 'When receiving fatal damage, there is a chance that you are instead restored to 25% of maximum Life and cause nearby enemies to flee in fear.'
+  },
+  {
+    name: 'Razeth\'s Volition',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'shoulders',
+      type: 'shoulders'
+    },
+    legendaryPower: 'Skeletal Mage gains the effect of the Gift of Death and Singularity runes. You gain [35 - 50%] damage reduction if your Essence is not full.'
+  },
+  {
+    name: 'Spaulders of Zakara',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'shoulders',
+      type: 'shoulders'
+    },
+    legendaryPower: 'Your items become indestructible. (Emanates)'
+  },
+  {
+    name: 'Vile Ward',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'shoulders',
+      type: 'shoulders'
+    },
+    legendaryPower: 'Furious Charge deals [30 - 35%] increased damage for every enemy hit while charging.'
+  },
+  {
+    name: 'Star Pauldrons',
+    class: 'generic',
+    quality: 'common',
+    equipment: {
+      slot: 'shoulders',
+      type: 'shoulders'
+    },
+    legendaryPower: 'Furious Charge deals [30 - 35%] increased damage for every enemy hit while charging.'
+  },
+]
