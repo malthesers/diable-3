2611dd05c2c0b1ed3520437afc0b54a0fddb74a4
malt9830 <malt9830@stud.kea.dk>
2023-11-03 17:28:30 +0100

    Added legendary + set mighty belts

--- src/data/waist/mightyBelts.ts
+++ src/data/waist/mightyBelts.ts
@@ -9,4 +9,73 @@ interface MightyBelt extends Item {
 }
 
 export const mightyBelts:MightyBelt[] = [
+  {
+    name: 'Chilanik\'s Chain',
+    class: 'barbarian',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'mighty belt'
+    },
+    legendaryPower: 'Using War Cry increases the movement speed for you and all allies affected by [30 - 40%] for 10 seconds.'
+  },
+  {
+    name: 'Dread Iron',
+    class: 'barbarian',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'mighty belt'
+    },
+    legendaryPower: 'Ground Stomp causes an Avalanche.'
+  },
+  {
+    name: 'Girdle of Giants',
+    class: 'barbarian',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'mighty belt'
+    },
+    legendaryPower: 'Seismic Slam increases Earthquake damage by [200 - 250%] for 3 seconds.'
+  },
+  {
+    name: 'Lamentation',
+    class: 'barbarian',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'mighty belt'
+    },
+    legendaryPower: 'Rend can now stack up to 2 times on an enemy, and the damage of Rend is increased by [100 - 150%].'
+  },
+  {
+    name: 'Pride of Cassius',
+    class: 'barbarian',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'mighty belt'
+    },
+    legendaryPower: 'Increases the duration of Ignore Pain by [4 - 6] seconds.'
+  },
+  {
+    name: 'The Undisputed Champion',
+    class: 'barbarian',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'mighty belt'
+    },
+    legendaryPower: 'Frenzy gains the effect of every rune and deals [300 - 400%] increased damage.'
+  },
+  {
+    name: 'Immortal King\'s Tribal Binding',
+    class: 'barbarian',
+    quality: 'set',
+    equipment: {
+      slot: 'waist',
+      type: 'mighty belt'
+    },
+  },
 ]
