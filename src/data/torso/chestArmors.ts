c9083bc08ed9fa9a445da5359105c25e7548aa0d
malt9830 <malt9830@stud.kea.dk>
2023-11-03 16:48:26 +0100

    Added set chest armor

--- src/data/torso/chestArmors.ts
+++ src/data/torso/chestArmors.ts
@@ -1,7 +1,7 @@
-import { Item } from '@/src/interfaces/Item'
+import { Classes, Item } from '@/src/interfaces/Item'
 
 interface ChestArmor extends Item {
-  class: 'generic',
+  class: Classes,
   equipment: {
     slot: 'torso',
     type: 'chest armor'
@@ -131,6 +131,321 @@ export const chestArmors:ChestArmor[] = [
     name: 'Godly Plate of the Whale',
     class: 'generic',
     quality: 'magic',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Aughild\'s Rule',
+    class: 'generic',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Born\'s Frozen Soul',
+    class: 'generic',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Demon\'s Marrow',
+    class: 'generic',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Blackthorne\'s Surcoat',
+    class: 'generic',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Markings of Savages',
+    class: 'barbarian',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Immortal King\'s Eternal Reign',
+    class: 'barbarian',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Spirit of the Earth',
+    class: 'barbarian',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Raekor\'s Heart',
+    class: 'barbarian',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Cuirass of the Wastes',
+    class: 'barbarian',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Brigandine of Valor',
+    class: 'crusader',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Breastplate of Akkhan',
+    class: 'crusader',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Roland\'s Bearing',
+    class: 'crusader',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Heart of the Light',
+    class: 'crusader',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Marauder\'s Carapace',
+    class: 'demon hunter',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'The Shadow\'s Bane',
+    class: 'demon hunter',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Lamellars of Justice',
+    class: 'monk',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Sunwuko\'s Soul',
+    class: 'monk',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Heart of the Crashing Wave',
+    class: 'monk',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Uliana\'s Heart',
+    class: 'monk',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Inna\'s Vast Expanse',
+    class: 'monk',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Rathma\'s Ribcage Plate',
+    class: 'necromancer',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Inarius\'s Conviction',
+    class: 'necromancer',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Sophisticated Vest',
+    class: 'necromancer',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Pestilence Robe',
+    class: 'necromancer',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Trag\'Oul\'s Scales',
+    class: 'necromancer',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Helltooth Tunic',
+    class: 'witch doctor',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Jade Harvester\'s Peace',
+    class: 'witch doctor',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Arachyr\'s Carapace',
+    class: 'witch doctor',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Mundunugu\'s Robe',
+    class: 'witch doctor',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Zunimasssa\'s Marrow',
+    class: 'witch doctor',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Firebird\'s Breast',
+    class: 'wizard',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Harness of Truth',
+    class: 'wizard',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Typhon\'s Thorax',
+    class: 'wizard',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Vyr\'s Astonishing Aura',
+    class: 'wizard',
+    quality: 'set',
+    equipment: {
+      slot: 'torso',
+      type: 'chest armor'
+    },
+  },
+  {
+    name: 'Tal Rasha\'s Relentless Pursuit',
+    class: 'wizard',
+    quality: 'set',
     equipment: {
       slot: 'torso',
       type: 'chest armor'
