5244296e8083e2011ec849223c33320648f67168
malt9830 <malt9830@stud.kea.dk>
2023-11-03 17:44:54 +0100

    Added legendary belts

--- src/data/waist/belts.ts
+++ src/data/waist/belts.ts
@@ -9,4 +9,373 @@ interface Belt extends Item {
 }
 
 export const belts:Belt[] = [
+  {
+    name: 'Angel Hair Braid',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Punish gains the effect of every rune.'
+  },
+  {
+    name: 'Bakuli Jungle Wraps',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Firebats deals [250 - 300%] increased damage to enemies affected by Locust Swarm or Piranhas.'
+  },
+  {
+    name: 'Belt of the Trove',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Every 4 seconds, call down Bombardment on a random nearby enemy. Bombardment deals [400 - 500%] increased damage.'
+  },
+  {
+    name: 'Belt of Transcendence',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Summon a Fetish Sycophant when you hit with a Mana spender.'
+  },
+  {
+    name: 'Binding of the Lost',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Each hit with Seven-Sided Strike grants [4.0 - 5.0%] damage reduction for 7 seconds.'
+  },
+  {
+    name: 'Blessed of Haull',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Justice spawns a Blessed Hammer when it hits an enemy.'
+  },
+  {
+    name: 'Brood of Araneae',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Corpse Spiders deals an additional [75 - 100%] damage, and each spider bite causes the target to take an additional 1% damage from Corpse Spiders for 5 seconds.'
+  },
+  {
+    name: 'Chain of Shadows',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'After using Impale, Vault costs no resource for 2 seconds.'
+  },
+  {
+    name: 'Cord of the Sherma',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Chance on hit to create a chaos field that Blinds and Slows enemies inside for [3 - 4] seconds.'
+  },
+  {
+    name: 'Crashing Rain',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Rain of Vengeance also summons a crashing beast that deals [3000 - 4000%] weapon damage.'
+  },
+  {
+    name: 'Dayntee\'s Binding',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'You gain an additional [40 - 50%] damage reduction when there is an enemy afflicted by any of your curses.'
+  },
+  {
+    name: 'Fazula\'s Improbable Chain',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'You automatically start with [40 - 50] Archon stacks when entering Archon form.'
+  },
+  {
+    name: 'Fleeting Strap',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+  },
+  {
+    name: 'Goldwrap',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'On gold pickup: Gain armor for 5 seconds equal to the amount picked up.'
+  },
+  {
+    name: 'Harrington Waistguard',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Opening a chest grants [100 - 135%] increased damage for 10 seconds.'
+  },
+  {
+    name: 'Haunting Girdle',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Haunt releases [1] extra spirit.'
+  },
+  {
+    name: 'Hellcat Waistguard',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Grenades have a chance to bounce [3 - 5] times dealing an additional 50% damage on each bounce. This bonus is increased to 800% on the final bounce.'
+  },
+  {
+    name: 'Hergbrash\'s Binding',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Reduces the Arcane Power cost of Arcane Torrent, Disintegrate, and Ray of Frost by [50 - 65%].'
+  },
+  {
+    name: 'Hunter\'s Wrath',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Your primary skills attack 30% faster and deal [150 - 200%] increased damage.'
+  },
+  {
+    name: 'Hwoj Wrap',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Locust Swarm also Slows enemies by [60 - 80%].'
+  },
+  {
+    name: 'Insatiable Belt',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Picking up a Health Globe increases your maximum Life by 5% for 15 seconds, stacking up to 5 times.'
+  },
+  {
+    name: 'Jang\'s Envelopment',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Enemies damaged by Black Hole are also slowed by [60 - 80%] for 3 seconds.'
+  },
+  {
+    name: 'Khassett\'s Cord of Righteousness',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Fist of the Heavens costs 40% less Wrath and deals [130 - 170%] more damage.'
+  },
+  {
+    name: 'Kyoshiro\'s Soul',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Sweeping Wind gains [2] stacks every second it does not deal damage to any enemies.'
+  },
+  {
+    name: 'Omnislash',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Slash attacks in all directions.'
+  },
+  {
+    name: 'Omryn\'s Chain',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Drop Caltrops when using Vault.'
+  },
+  {
+    name: 'Razor Strop',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Picking up a Health Globe releases an explosion that deals [300 - 400%] weapon damage as Fire to enemies within 20 yards.'
+  },
+  {
+    name: 'Sacred Harness',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Judgment gains the effect of the Debilitate rune and is cast at your landing location when casting Falling Sword.'
+  },
+  {
+    name: 'Saffron Wrap',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'The damage of your next Overpower is increased by [40 - 50%] for each enemy hit. Max 20 enemies.'
+  },
+  {
+    name: 'Sash of Knives',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'With every attack, you throw a dagger at a nearby enemy for [500 - 650%] weapon damage as Physical.'
+  },
+  {
+    name: 'Sebor\'s Nightmare',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Haunt is cast on all nearby enemies when you open a chest.'
+  },
+  {
+    name: 'String of Ears',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Reduces damage from melee attacks by [25 - 30%].'
+  },
+  {
+    name: 'The Shame of Delsere',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Your Signature Spells attack 50% faster and restore [9 - 12] Arcane Power.'
+  },
+  {
+    name: 'The Witching Hour',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+  },
+  {
+    name: 'Thundergod\'s Vigor',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'Blocking, dodging or being hit causes you to discharge bolts of electricity that deal [100 - 130%] weapon damage as Lightning.',
+    elementalDamage: 'lightning'
+  },
+  {
+    name: 'Vigilante Belt',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'While Steed Charge is active, Fist of the Heavens deals increased damage equal to your movement speed, and is automatically called down on a random nearby enemy at an interval based on your attack speed if it is on your action bar.'
+  },
+  {
+    name: 'Zoey\'s Secret',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'waist',
+      type: 'belt'
+    },
+    legendaryPower: 'You take [8.0 - 9.0%] less damage for every Companion you have active.'
+  },
 ]
