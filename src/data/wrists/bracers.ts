83c2c7072456d3fc3117a34f7238815efd9c7dca
malt9830 <malt9830@stud.kea.dk>
2023-11-03 17:03:35 +0100

    Added legendary bracers

---
+++ src/data/wrists/bracers.ts
@@ -0,0 +1,260 @@
+import { Classes, Item } from '@/src/interfaces/Item';
+
+interface Bracers extends Item {
+  class: Classes,
+  equipment: {
+    slot: 'wrists',
+    type: 'bracers'
+  }
+}
+
+export const bracersItems:Bracers[] = [
+  {
+    name: 'Akkhan\'s Manacles',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Blessed Shield damage is increased by [400 - 500%] for the first enemy it hits.'
+  },
+  {
+    name: 'Ancient Parthan Defenders',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Each stunned enemy within 25 yards reduces your damage taken by [9 - 12%].'
+  },
+  {
+    name: 'Ashnagarr\'s Blood Bracer',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Increases the potency of your shields by [75 - 100%].'
+  },
+  {
+    name: 'Bindings of the Lesser Gods',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Enemies hit by your Cyclone Strike take [150 - 200%] increased damage from your Mystic Ally for 5 seconds. Split Fire Allies gain 3 times this bonus.'
+  },
+  {
+    name: 'Bonds of C\'Lena',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Army of the Dead deals [75 - 100%] increased damage or double this bonus against an active target of Command Skeletons.'
+  },
+  {
+    name: 'Bracer of Fury',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Heaven\'s Fury deals [300 - 400%] increased damage to enemies that are Blinded, Immobilized, or Stunned.'
+  },
+  {
+    name: 'Bracers of Destruction',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Seismic Slam deals [400 - 500%] increased damage to the first 10 enemies it hits.'
+  },
+  {
+    name: 'Bracers of the First Men',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Hammer of the Ancients attacks 50% faster and deals [375 - 500%] increased damage.'
+  },
+  {
+    name: 'Cesar\'s Memento',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Enemies take [600 - 800%] increased damage from your Tempest Rush for 5 seconds after you hit them with a Blind, Freeze, or Stun.'
+  },
+  {
+    name: 'Coil\'s of the First Spider',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'While channeling Firebats, you gain 30% damage reduction and [60,000 - 80,000] Life per Hit.'
+  },
+  {
+    name: 'Custerian Wristguards',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Picking up gold grants experience. (Emanates)'
+  },
+  {
+    name: 'Drakon\'s Lesson',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'When your Shield Bash hits 3 or fewer enemies, its damage is increased by [300 - 400%] and 25% of its Wrath Cost is refunded.'
+  },
+  {
+    name: 'Gabriel\'s Vambraces',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'When your Blessed Hammer hits 3 or fewer enemies, [75 - 100%] of its Wrath Cost is refunded.'
+  },
+  {
+    name: 'Gelmindor\'s Marrow Guards',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Bone Spears cast from Simulacrums deal [300 - 400%] increased damage.'
+  },
+  {
+    name: 'Gungdo Gear',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Exploding Palm\'s on-death explosion applies Exploding Palm. Exploding Palm\'s damage is increased by [75 - 100%].'
+  },
+  {
+    name: 'Jeram\'s Bracers',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Wall of Death deals [75 - 100%] increased damage and can be cast up to three times within 2 seconds before the cooldown begins.'
+  },
+  {
+    name: 'Kethrye\'s Splint',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+  },
+  {
+    name: 'Lacuni Prowlers',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+  },
+  {
+    name: 'Lakumba\'s Ornament',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Reduce all damage taken by 60% if your Soul Harvest stack is at least [1 - 3], and an additional 2% for each stack of Soul Harvest.'
+  },
+  {
+    name: 'Mortick\'s Brace',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Wrath of the Berserker gains the effect of every rune.'
+  },
+  {
+    name: 'Nemesis Bracers',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Shrines and Pylons will spawn an enemy champion. (Emanates)'
+  },
+  {
+    name: 'Pinto\'s Pride',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Wave of Light also Slows enemies by 80% for 3 seconds and deals [125 - 150%] increased damage.'
+  },
+  {
+    name: 'Promise of Glory',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: '[4 - 6%] chance to spawn a Nephalem Glory globe when you Blind an enemy.'
+  },
+  {
+    name: 'Ranslor\'s Folly',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'The damage of Energy Twister is increased by [225 - 300%] and it periodically pulls in lesser enemies within 30 yards.'
+  },
+  {
+    name: 'Reaper\'s Wraps',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'wrists',
+      type: 'bracers'
+    },
+    legendaryPower: 'Health globes restore [25 - 30%] of your primary resource. This effect cannot occur more than once every 2 seconds.'
+  },
+]
