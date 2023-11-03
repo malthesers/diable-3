fd8d14b5a217bc4f9e29ceb7dbdb81de96f34c99
malt9830 <malt9830@stud.kea.dk>
2023-11-03 12:06:28 +0100

    Installed Fuse.js

--- src/components/game/search/SearchContainer.tsx
+++ src/components/game/search/SearchContainer.tsx
@@ -3,13 +3,25 @@ import { Item } from '@/src/interfaces/Item'
 import { useRef } from 'react'
 import SearchResults from './SearchResults'
 import SearchBar from './SearchBar'
+import Fuse from 'fuse.js'
 
 export default function SearchContainer() {
   const { items, chosen, guesses, validateGuess, search, setSearch } = useItems()
   const ref = useRef<HTMLInputElement | null>(null)
+
   const eligible:Item[] = items.filter(item => chosen[item.quality as keyof typeof chosen])
   const remaining:Item[] = eligible.filter(item =>  !guesses.some(guess => guess.name === item.name))
+
   const results:Item[] = remaining.filter(item => item.name.toLowerCase().includes(search.toLowerCase())).slice(0, 10)
+
+  const fuse = new Fuse(remaining, {
+    keys: ['name'],
+  })
+
+  const resulters = fuse.search('sword', {
+    limit: 10
+  })
+  console.log(resulters)
 
   function submitGuess(item:Item) {
     if (ref.current) ref.current.focus()
