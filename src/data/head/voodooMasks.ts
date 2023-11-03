178137cf47afe17c1a697be06d6114faebe69e9d
malt9830 <malt9830@stud.kea.dk>
2023-11-03 14:39:39 +0100

    Added legendary + set voodoo masks

--- src/data/head/voodooMasks.ts
+++ src/data/head/voodooMasks.ts
@@ -9,4 +9,85 @@ interface VoodooMask extends Item {
 }
 
 export const voodooMasks:VoodooMask[] = [
+  {
+    name: 'Carnevil',
+    class: 'witch doctor',
+    quality: 'legendary',
+    equipment: {
+      slot: 'head',
+      type: 'voodoo mask'
+    },
+  },
+  {
+    name: 'Mask of Jeram',
+    class: 'witch doctor',
+    quality: 'legendary',
+    equipment: {
+      slot: 'head',
+      type: 'voodoo mask'
+    },
+  },
+  {
+    name: 'Quetzalcoatl',
+    class: 'witch doctor',
+    quality: 'legendary',
+    equipment: {
+      slot: 'head',
+      type: 'voodoo mask'
+    },
+  },
+  {
+    name: 'Split Tusk',
+    class: 'witch doctor',
+    quality: 'legendary',
+    equipment: {
+      slot: 'head',
+      type: 'voodoo mask'
+    },
+  },
+  {
+    name: 'The Grin Reaper',
+    class: 'witch doctor',
+    quality: 'legendary',
+    equipment: {
+      slot: 'head',
+      type: 'voodoo mask'
+    },
+  },
+  {
+    name: 'Tiklandian Visage',
+    class: 'witch doctor',
+    quality: 'legendary',
+    equipment: {
+      slot: 'head',
+      type: 'voodoo mask'
+    },
+  },
+  {
+    name: 'Visage of Giyua',
+    class: 'witch doctor',
+    quality: 'legendary',
+    equipment: {
+      slot: 'head',
+      type: 'voodoo mask'
+    },
+  },
+  {
+    name: 'Mundunugu\'s Headdress',
+    class: 'witch doctor',
+    quality: 'set',
+    equipment: {
+      slot: 'head',
+      type: 'voodoo mask'
+    },
+  },
+  {
+    name: 'Zunimassa\'s Vision',
+    class: 'witch doctor',
+    quality: 'set',
+    equipment: {
+      slot: 'head',
+      type: 'voodoo mask'
+    },
+  },
 ]
