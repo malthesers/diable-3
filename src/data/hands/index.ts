51bcf703db084af9725f3fbd10ca353e1c2dac07
malt9830 <malt9830@stud.kea.dk>
2023-11-03 17:22:01 +0100

    Added set gloves

--- src/data/hands/index.ts
+++ src/data/hands/index.ts
@@ -110,4 +110,346 @@ export const handsItems:Gloves[] = [
     },
     legendaryPower: 'Increase attack speed of your pets by [40 - 50%]',
   },
+  {
+    name: 'Sage\'s Purchase',
+    class: 'generic',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Cain\'s Scrivener',
+    class: 'generic',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Asheara\'s Ward',
+    class: 'generic',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Claws of Savages',
+    class: 'barbarian',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Immortal King\'s Irons',
+    class: 'barbarian',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Pull of the Earth',
+    class: 'barbarian',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Raekor\'s Wraps',
+    class: 'barbarian',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Gauntlet of the Wastes',
+    class: 'barbarian',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Gauntlets of Valor',
+    class: 'crusader',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Gauntlets of Akkhan',
+    class: 'crusader',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Roland\'s Grasp',
+    class: 'crusader',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Pride of the Invoker',
+    class: 'crusader',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Will of the Light',
+    class: 'crusader',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Fiendish Grips',
+    class: 'demon hunter',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Gas Powered Automail Forearm',
+    class: 'demon hunter',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Marauder\'s Gloves',
+    class: 'demon hunter',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Natalya\'s Touch',
+    class: 'demon hunter',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'The Shadow\'s Grasp',
+    class: 'demon hunter',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Bazubands of Justice',
+    class: 'monk',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Sunwuko\'s Paws',
+    class: 'monk',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Fists of Thunder',
+    class: 'monk',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Uliana\'s Fury',
+    class: 'monk',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Inna\'s Hold',
+    class: 'monk',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Rathma\'s Macabre Vambracers',
+    class: 'necromancer',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Inarius\'s Will',
+    class: 'necromancer',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Lavishing Gloves',
+    class: 'necromancer',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Pestilence Gloves',
+    class: 'necromancer',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Trag\'Oul\'s Claws',
+    class: 'necromancer',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Helltooth Gauntlets',
+    class: 'witch doctor',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Jade Harvester\'s Mercy',
+    class: 'witch doctor',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Arachyr\'s Claws',
+    class: 'witch doctor',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Mundunugu\'s Rhythm',
+    class: 'witch doctor',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Zunimasssa\'s Finger Wraps',
+    class: 'witch doctor',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Firebird\'s Talons',
+    class: 'wizard',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Fierce Gauntlets',
+    class: 'wizard',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Typhon\'s Claws',
+    class: 'wizard',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Vyr\'s Grasping Gauntlets',
+    class: 'wizard',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
+  {
+    name: 'Tal Rasha\'s Grasp',
+    class: 'wizard',
+    quality: 'set',
+    equipment: {
+      slot: 'hands',
+      type: 'gloves'
+    },
+  },
 ]
