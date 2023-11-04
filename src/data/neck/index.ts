24d77d09493679bc50dba8a8d7be4ed0c46374fe
malt9830 <malt9830@stud.kea.dk>
2023-11-04 06:27:17 +0100

    Added legendary amulets

--- src/data/neck/index.ts
+++ src/data/neck/index.ts
@@ -9,4 +9,252 @@ interface Amulet extends Item {
 }
 
 export const neckItems:Amulet[] = [
+  {
+    name: 'Ancestors\' Grace',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'When receiving fatal damage, you are instead restored to 100% of maximum Life and resources. This item is destroyed in the process.'
+  },
+  {
+    name: 'Countess Julia\'s Cameo',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Prevent all Arcane damage taken and heal yourself for [20 - 25%] of the amount prevented.'
+  },
+  {
+    name: 'Dovu Energy Trap',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Increases duration of Stun effects by [20 - 25%]. (Emanates)'
+  },
+  {
+    name: 'Eye of Etlich',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+  },
+  {
+    name: 'Golden Gorget of Leoric',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'After earning a massacre bonus, 6 Skeletons are summoned to fight by your side for 10 seconds.'
+  },
+  {
+    name: 'Halcyon\'s Ascent',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'When you use class ability, you mesmerize nearby enemies with your skill, causing them to jump uncontrollably for [6 - 8] seconds.'
+  },
+  {
+    name: 'Haunt of Vaxo',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Summons shadow clones to your aid when you Stun an enemy. This effect may occur once every [30] seconds.'
+  },
+  {
+    name: 'Haunted Visions',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Simulacrum now drains [1%] of your maximum life every second and lasts forever. They can now duplicate Grim Scythe and can no longer cast Skeletal Mage.'
+  },
+  {
+    name: 'Hellfire Amulet',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Random class passive.'
+  },
+  {
+    name: 'Holy Beacon',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    elementalDamage: 'holy'
+  },
+  {
+    name: 'Kymbo\'s Gold',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Picking up gold heals you for an amount equal to the gold that was picked up.'
+  },
+  {
+    name: 'Mara\'s Kaleidoscope',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Prevent all Poison damage taken and heal yourself for [10 - 15%] of the amount prevented.'
+  },
+  {
+    name: 'Moonlight Ward',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Hitting an enemy within 15 yards has a chance to ward you with shards of Arcane energy that explode when enemies get close, dealing [240 - 320%] weapon damage as Arcane to enemies within 15 yards.',
+    elementalDamage: 'arcane'
+  },
+  {
+    name: 'Ouroboros',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+  },
+  {
+    name: 'Overwhelming Desire',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Chance on hit to charm the enemy. While charmed, the enemy takes 35% increased damage.'
+  },
+  {
+    name: 'Rakoff\'s Glass of Life',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Enemies you kill have a [3 - 4%] additional chance to drop a health globe. (Emanates)'
+  },
+  {
+    name: 'Rondal\'s Locket',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+  },
+  {
+    name: 'Squirt\'s Necklace',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'While not taking damage, damage dealt is increased by up to 100% and damage taken is increased by up to 50%.'
+  },
+  {
+    name: 'Talisman of Aranoch',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Prevent all Cold damage taken and heal yourself for [10 - 15%] of the amount prevented.'
+  },
+  {
+    name: 'The Ess of Johan',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Chance on hit to pull in enemies toward your target and Slow them by [60 - 80%].'
+  },
+  {
+    name: 'The Flavor of Time',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Pylon effects last twice as long. (Emanates)'
+  },
+  {
+    name: 'The Johnstone',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'When Land of the Dead expires, you are granted 50 stacks of Macabre Knowledge. Macabre Knowledge increases the damage of Corpse Lance and Corpse Explosion by [150 - 200%].'
+  },
+  {
+    name: 'The Star of Azkaranth',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Prevent all Fire damage taken and heal yourself for [10 - 15%] of the amount prevented.'
+  },
+  {
+    name: 'Wisdom of Kalan',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Increases the maximum stacks of Bone Armor by [5].'
+  },
+  {
+    name: 'Xephirian Amulet',
+    class: 'generic',
+    quality: 'legendary',
+    equipment: {
+      slot: 'neck',
+      type: 'amulet'
+    },
+    legendaryPower: 'Prevent all Lightning damage taken and heal yourself for [10 - 15%] of the amount prevented.'
+  },
 ]
