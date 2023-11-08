72aeea1b05b434187775b1bd3823c160526a9314
malt9830 <malt9830@stud.kea.dk>
2023-11-08 16:03:17 +0100

    Fixed typo in InstructionsModal

--- src/components/modals/InstructionsModal.tsx
+++ src/components/modals/InstructionsModal.tsx
@@ -7,7 +7,7 @@ export default function InstructionsModal() {
   return (
     <ModalTemplate showModal={showInstructions} closeModal={() => toggleShowInstructions(false)} title='Instructions'>
       <div className='text-lg sm:text-xl text-center space-y-2'>
-        <p>Search for an item and enter your guess. View guess to find correct and incorrect properties.</p>
+        <p>Search for an item and enter your guess. View your guess to find correct and incorrect properties.</p>
         <p>A <span className='font-semibold text-green-600'>green</span> background indicates a correct value, and a <span className='font-semibold text-red-600'>red</span> background indicates an incorrect value.</p>
         <p>Revealed properties appear in the item tooltip. After 10 guesses, a blurred <span className='font-semibold text-legendary'>legendary power</span> will also appear, and be revealed on hover.</p>
         <p><span className='font-semibold'>Common</span> and <span className='font-semibold text-magic'>magic</span> items constitute only <span className='italic'>unique</span> items, e.g. transmogs commons and Darkening of Tristram magic items.</p>
