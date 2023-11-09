fb29621e53f6db8ccb742076a82a50b477ab54d4
malt9830 <malt9830@stud.kea.dk>
2023-11-09 17:59:38 +0100

    Added RecordsModal descriptive text

--- src/components/modals/RecordsModal.tsx
+++ src/components/modals/RecordsModal.tsx
@@ -51,10 +51,13 @@ export default function RecordsModal() {
 
   return (
     <ModalTemplate showModal={showRecords} closeModal={() => toggleShowRecords(false)} title='Records'>
-      <div className='text-lg sm:text-xl text-center space-y-2'>
-        { records.map(record =>
-          <RecordItem key={record.id} record={record}/>
-        )}
+      <div className='text-base sm:text-xl text-center space-y-2'>
+        <p>Below is a list of your fastest victories including the guessed item, the amount of guesses and the included qualities.</p>
+        <div className='grid gap-2'>
+          { records.map(record =>
+            <RecordItem key={record.id} record={record}/>
+          )}
+        </div>
       </div>
     </ModalTemplate>
   )
