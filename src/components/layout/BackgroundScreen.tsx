cb411160fd137e4d759b80a7553bb1cbeb886c5c
malt9830 <malt9830@stud.kea.dk>
2023-12-01 16:47:15 +0100

    Added BackgroundScreen component

---
+++ src/components/layout/BackgroundScreen.tsx
@@ -0,0 +1,5 @@
+export default function BackgroundScreen() {
+  return (
+    <div className="-z-10 w-full h-[100dvh] bg-[url('/images/background.jpg')] bg-no-repeat bg-cover bg-center"></div>
+  )
+}
