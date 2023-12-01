5bdc21bbc13988637a2e1c66846658e0f4667e2f
malt9830 <malt9830@stud.kea.dk>
2023-12-01 18:56:55 +0100

    Moved screenshots to public and updated manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,25 +14,57 @@ export default function manifest(): MetadataRoute.Manifest {
     categories: ["entertainment", "games"],
     icons: [
       {
-        'src': '/icon1.png',
-        'sizes': '16x16',
-        'type': 'image/png'
+        src: '/icon1.png',
+        sizes: '16x16',
+        type: 'image/png'
       },
       {
-        'src': '/icon2.png',
-        'sizes': '32x32',
-        'type': 'image/png'
+        src: '/icon2.png',
+        sizes: '32x32',
+        type: 'image/png'
       },
       {
-        'src': '/icon3.png',
-        'sizes': '192x192',
-        'type': 'image/png'
+        src: '/icon3.png',
+        sizes: '192x192',
+        type: 'image/png'
       },
       {
-        'src': '/icon4.png',
-        'sizes': '512x512',
-        'type': 'image/png'
+        src: '/icon4.png',
+        sizes: '512x512',
+        type: 'image/png'
       },
+    ],
+    screenshots: [
+      {
+        src: "/screenshots/mobile1.jpg",
+        type: "image/png",
+        sizes: "1170x1900",
+      },
+      {
+        src: "/screenshots/mobile2.jpg",
+        type: "image/png",
+        sizes: "1170x1900",
+      },
+      {
+        src: "/screenshots/mobile3.jpg",
+        type: "image/png",
+        sizes: "1170x1900",
+      },
+      {
+        src: "/screenshots/desktop1.jpg",
+        type: "image/png",
+        sizes: "1074x620",
+      },
+      {
+        src: "/screenshots/desktop2.jpg",
+        type: "image/png",
+        sizes: "1074x620",
+      },
+      {
+        src: "/screenshots/desktop3.jpg",
+        type: "image/png",
+        sizes: "1074x620",
+      }
     ]
   }
 }
