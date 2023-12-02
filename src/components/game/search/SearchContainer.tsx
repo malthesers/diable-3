f29ecf98a387a632d74fa9a537a4ba7463d83dd6
malt9830 <malt9830@stud.kea.dk>
2023-12-02 05:40:14 +0100

    Input field is now focused on new game

--- src/components/game/search/SearchContainer.tsx
+++ src/components/game/search/SearchContainer.tsx
@@ -1,23 +1,31 @@
 import { useItems } from '@/src/context/ItemsProvider'
 import { Item } from '@/src/interfaces/Item'
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import SearchResults from './SearchResults'
 import SearchBar from './SearchBar'
 
 export default function SearchContainer() {
-  const { validateGuess, setSearch } = useItems()
+  const { answer, validateGuess, setSearch } = useItems()
   const ref = useRef<HTMLInputElement | null>(null)
 
-  function submitGuess(item:Item) {
+  function focusInput() {
     if (ref.current && screen.width > 669) ref.current.focus()
+  }
+
+  function submitGuess(item:Item) {
     delete item.ref
     validateGuess(item)
     setSearch('')
+    focusInput()
   }
+
+  useEffect(() => {
+    focusInput()
+  }, [answer])
 
   return (
     <div className='relative mb-2'>
-      <SearchBar ref={ref}/>
+      <SearchBar ref={ref} />
       <SearchResults submitGuess={submitGuess}/>
     </div>
   )
