d210118df749e5b5339126d71f4ce2ade90ba192
malt9830 <malt9830@stud.kea.dk>
2023-12-03 08:09:21 +0100

    Added optional disabled prop for Button component

--- src/components/layout/Button.tsx
+++ src/components/layout/Button.tsx
@@ -3,14 +3,16 @@ import { forwardRef } from 'react'
 interface ButtonProps {
   onClick: () => void,
   className?: string,
+  disabled?: boolean
   text: string,
 }
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button({ onClick, className, text} : ButtonProps, ref) {
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button({ onClick, className, disabled = false, text} : ButtonProps, ref) {
   return (
     <button
       ref={ref}
       onClick={onClick}
+      disabled={disabled}
       className={(className ? className : '') + ' w-36 xs:w-44 aspect-[159/41] bg-[url("/images/button.png")] bg-cover bg-center text-sm xs:text-base text-gold uppercase outline-none duration-200 hover:text-gold-light hover:brightness-125 focus-visible:text-gold-light focus-visible:brightness-125'}
     >
       {text}
